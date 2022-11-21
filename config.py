cc85a8df118a4c3bfbd4010e1236031971e785b7
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-21 21:34:44 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+class Config (object):
+    LOG=True
+
+    API_ID=16191628
+    API_HASH="7d5acccaf1df4f5b7a690b203fd1953e"
+    BOT_TOKEN="5918324070:AAHvKsRll72lGTwgPzQoaaEpLXlnZD71QnQ"
+
+
+class Friday (Config):
+    LOG=True 
