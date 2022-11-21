e555221ba3849ecd0474d5eaa7c013de2847f8fd
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-21 22:06:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config (object):
     API_HASH="7d5acccaf1df4f5b7a690b203fd1953e"
     BOT_TOKEN="5918324070:AAHvKsRll72lGTwgPzQoaaEpLXlnZD71QnQ"
     WORKERS = 8
+    URL=None
 
 
 class Friday (Config):
