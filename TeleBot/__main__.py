22b957954d2ab1eafc6aeba31203580311cdad08
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-21 21:40:17 +0530

    Update __main__.py

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -1,2 +1,11 @@
 from TeleBot import pgram
+from pyrogram import filters
+
+@app.on_message(filters.command("start"))
+async def start(_,msg):
+    await msg.reply_text("am started")
+
+if __name__ == "__main__":
+    LOG.info("started")
+    app.run()
 
