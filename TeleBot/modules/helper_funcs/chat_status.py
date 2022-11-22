48f2c0356e2941286022544eaece747908a74f39
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-23 04:49:31 +0530

    Update chat_status.py

--- TeleBot/modules/helper_funcs/chat_status.py
+++ TeleBot/modules/helper_funcs/chat_status.py
@@ -1,7 +1,7 @@
 from TeleBot import dispatcher
 from telegram import Chat, ChatMember, ParseMode, Update, TelegramError, User
 from telegram.ext import CallbackContext
-
+from cachetools import TTLCache
 # stores admemes in memory for 10 min.
 ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
 THREAD_LOCK = RLock()
