2405b9d5942ce3f5ded57a8ceaefa4fc9cd3e984
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 22:47:09 +0530

    Create imdb.py

---
+++ TeleBot/modules/imdb.py
@@ -0,0 +1,59 @@
+import requests
+from TeleBot import pgram as app
+from pyrogram import filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+@app.on_message(filters.command("imdb"))
+async def IMDb(_,msg):
+    if len(msg.command) < 2:        
+        await msg.reply_text("give me a query to search")
+    else:
+        pass 
+    text = (
+        msg.text.split(None, 1)[1]
+        if len(msg.command) < 3
+        else msg.text.split(None, 1)[1].replace(" ", "%20")
+    )
+    url=f"https://api.safone.me/tmdb?query={text}%20&limit=1"
+    re=requests.get(url).json()["results"][0]
+    title=re["title"]
+    poster=re["poster"]
+    runtime=re["runtime"]
+    rating=re["rating"]
+    releaseDate=re["releaseDate"]
+    genres=re["genres"][0:]
+    popularity=re["popularity"]
+    status=re["status"]
+    homepage=re["homepage"]
+    imdbId=re["imdbId"]
+    imdbLink=re["imdbLink"]
+    id=re["id"]        
+    overview=re["overview"]        
+    await msg.reply_photo(poster,
+    caption=f"""
+📀 **ᴛɪᴛʟᴇ :** {title}
+
+⏱️ **ʀᴜɴᴛɪᴍᴇ :** {runtime}ᴍɪɴ
+🌟 **ʀᴀᴛɪɴɢ :** {rating}/10
+🗳️ **ɪᴅ :** {id}
+
+📆 **ʀᴇʟᴇᴀsᴇ ᴅᴀᴛᴇ :** {releaseDate}
+🎭 **ɢᴇɴʀᴇ :** {genres}
+🥂 **ᴘᴏᴘᴜʟᴀʀɪᴛʏ :** {popularity}
+
+⚡ **sᴛᴀᴛᴜs :** {status}
+🎫 **ɪᴍᴅʙ ɪᴅ :** {imdbId}
+
+🗒  **ᴘʟᴏᴛ :** `{overview}`
+""",
+       reply_markup=InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="🎟️ 𝙸𝙼𝙳ʙ",
+                        url=imdbLink,
+                    ),
+                ],
+            ],
+        ),
+    )
