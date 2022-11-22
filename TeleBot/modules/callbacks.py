489ce5c379d62b361767f0478759f10068cbbb5a
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 20:55:51 +0530

    Update callbacks.py

--- TeleBot/modules/callbacks.py
+++ TeleBot/modules/callbacks.py
@@ -16,5 +16,5 @@ PM_START_TEXT = """
 async def Friday(_, callback_query : CallbackQuery):
     query= callback_query.message
     first_name=callback_query.from_user.first_name
-    uptime= get_readable_time(time.time() - StartTime)
+    uptime= get_readable_time(time.time() - StartTime))
     await query.edit_caption(PM_START_TEXT.format(first_name,BOT_NAME,uptime))
