3dd41734c412cfcf5c959c343e8f98717e91c0be
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-23 04:46:51 +0530

    Create chat_status.py

---
+++ TeleBot/modules/helper_funcs/chat_status.py
@@ -0,0 +1,32 @@
+from TeleBot import dispatcher
+from telegram import Chat, ChatMember, ParseMode, Update, TelegramError, User
+from telegram.ext import CallbackContext
+
+# stores admemes in memory for 10 min.
+ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
+THREAD_LOCK = RLock()
+
+
+def is_user_admin(chat: Chat, user_id: int, member: ChatMember = None) -> bool:
+    if (
+        chat.type == "private"        
+        or chat.all_members_are_administrators
+        or user_id in {1087968824}
+    ):  # Count telegram and Group Anonymous as admin
+        return True
+    if member:
+        return member.status in ("administrator", "creator")
+
+    with THREAD_LOCK:
+        # try to fetch from cache first.
+        try:
+            return user_id in ADMIN_CACHE[chat.id]
+        except KeyError:
+            # keyerror happend means cache is deleted,
+            # so query bot api again and return user status
+            # while saving it in cache for future useage...
+            chat_admins = dispatcher.bot.getChatAdministrators(chat.id)
+            admin_list = [x.user.id for x in chat_admins]
+            ADMIN_CACHE[chat.id] = admin_list
+
+            return user_id in admin_list
