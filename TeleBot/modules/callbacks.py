c957cb054be097c1557d64051e2d39c9b74b4ca6
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 21:00:18 +0530

    Update callbacks.py

--- TeleBot/modules/callbacks.py
+++ TeleBot/modules/callbacks.py
@@ -2,17 +2,19 @@ import time
 from pyrogram import filters 
 from TeleBot import pgram,StartTime,BOT_NAME,get_readable_time
 from pyrogram.types import CallbackQuery 
+from TeleBot.__main__ import PM_START_TEXT
 
 
-PM_START_TEXT = """
-🥀 ʜᴇʏ *{}* ,
+#PM_START_TEXT = """
+#🥀 ʜᴇʏ *{}* ,
         
-ᴛʜɪs ɪs *{}*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
-๏ ɪ'ᴍ ᴜɴᴅᴇʀ ᴄʀᴇᴀᴛɪᴏɴ ʙʏ ⸢[𝚂𝚃𝙰𝚁𝙺](https://t.me/NoobStark_21)⸥
-๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅsn.
-๏ *ᴅɪᴅɴ'ᴛ sʟᴇᴇᴘᴛ sɪɴᴄᴇ* {}
-"""
+#ᴛʜɪs ɪs *{}*
+#➖➖➖➖➖➖➖➖➖➖➖➖➖
+#๏ ɪ'ᴍ ᴜɴᴅᴇʀ ᴄʀᴇᴀᴛɪᴏɴ ʙʏ ⸢[𝚂𝚃𝙰𝚁𝙺](https://t.me/NoobStark_21)⸥
+#๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅsn.
+#๏ *ᴅɪᴅɴ'ᴛ sʟᴇᴇᴘᴛ sɪɴᴄᴇ* {}
+#"""
+
 @pgram.on_callback_query(filters.regex("friday_back"))
 async def Friday(_, callback_query : CallbackQuery):
     query= callback_query.message
