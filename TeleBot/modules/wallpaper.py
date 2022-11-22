6b61c79e2247d424904795e585ed02ec5a10fae2
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 23:10:18 +0530

    Create wallpaper.py

---
+++ TeleBot/modules/wallpaper.py
@@ -0,0 +1,23 @@
+import requests
+from TeleBot import pgram
+from pyrogram import filters
+
+@pgram.on_message(filters.command("wall"))
+async def wall(_,msg):
+    if len(msg.command) < 2:
+        await msg.reply_text("give something to search")
+    else:
+        pass
+    query=(
+       msg.text.split(None,1)[1]
+       if len(msg.commamd) < 3
+       else msg.text.split(None,1)[1].replace(" ","%20")
+       )
+    url=f"https://api.safone.me/wall?query={query}&limit=1"
+    re=requests.get(url).json()["results"][0]
+    pic=re["imageUrl"]
+    preview=re["thumbUrl"]
+    title=re["title"]
+    await pgram.send_photo(msg.chat.id,preview)
+    await pgram.send_document(msg.chat.id,pic)
+        
