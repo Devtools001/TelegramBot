f2f997530adba9980563619f83b6d81310a2fa36
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 21:43:29 +0530

    Update __init__.py

--- TeleBot/modules/__init__.py
+++ TeleBot/modules/__init__.py
@@ -1,24 +1,26 @@
-from TeleBot import LOAD,LOG,NO_LOAD
+from TeleBot import LOAD, LOGGER, NO_LOAD
 
-def _list_all_modules():
+
+def __list_all_modules():
     import glob
-    from os.path import basename,dirname,isfile
- 
+    from os.path import basename, dirname, isfile
+
+    # This generates a list of modules in this folder for the * in __main__ to work.
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
     all_modules = [
         basename(f)[:-3]
         for f in mod_paths
-        if isfile(f) and f.endswith(".py") and not f.endswith("__init__.py")
+        if isfile(f) and f.endswith(".py") and not f.endswith('__init__.py')
     ]
 
     if LOAD or NO_LOAD:
         to_load = LOAD
         if to_load:
             if not all(
-                any(mod == module_name for module_name in all_modules)
-                for mod in to_load
-            ):
-                LOG.error("Invalid loadorder names, Quitting...")
+                    any(mod == module_name
+                        for module_name in all_modules)
+                    for mod in to_load):
+                LOGGER.error("Invalid loadorder names. Quitting.")
                 quit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
@@ -28,7 +30,7 @@ def _list_all_modules():
             to_load = all_modules
 
         if NO_LOAD:
-            LOG.info("Not loading: {}".format(NO_LOAD))
+            LOGGER.info("Not loading: {}".format(NO_LOAD))
             return [item for item in to_load if item not in NO_LOAD]
 
         return to_load
@@ -36,6 +38,6 @@ def _list_all_modules():
     return all_modules
 
 
-ALL_MODULES = _list_all_modules()
-LOG.info("Modules to load: %s", str(ALL_MODULES))
+ALL_MODULES = __list_all_modules()
+LOGGER.info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
