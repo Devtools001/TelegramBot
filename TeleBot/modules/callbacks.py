8b334f9e7acf26c275509f8718a1961930e5b00b
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 21:02:38 +0530

    Update callbacks.py

--- TeleBot/modules/callbacks.py
+++ TeleBot/modules/callbacks.py
@@ -1,23 +1,16 @@
 import time
 from pyrogram import filters 
 from TeleBot import pgram,StartTime,BOT_NAME,get_readable_time
-from pyrogram.types import CallbackQuery 
-from TeleBot.__main__ import PM_START_TEXT
+from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton 
+from TeleBot.__main__ import PM_START_TEXT, buttons 
 
 
-#PM_START_TEXT = """
-#🥀 ʜᴇʏ *{}* ,
-        
-#ᴛʜɪs ɪs *{}*
-#➖➖➖➖➖➖➖➖➖➖➖➖➖
-#๏ ɪ'ᴍ ᴜɴᴅᴇʀ ᴄʀᴇᴀᴛɪᴏɴ ʙʏ ⸢[𝚂𝚃𝙰𝚁𝙺](https://t.me/NoobStark_21)⸥
-#๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅsn.
-#๏ *ᴅɪᴅɴ'ᴛ sʟᴇᴇᴘᴛ sɪɴᴄᴇ* {}
-#"""
+
 
 @pgram.on_callback_query(filters.regex("friday_back"))
 async def Friday(_, callback_query : CallbackQuery):
     query= callback_query.message
     first_name=callback_query.from_user.first_name
     uptime= get_readable_time((time.time() - StartTime))
-    await query.edit_caption(PM_START_TEXT.format(first_name,BOT_NAME,uptime))
+    await query.edit_caption(PM_START_TEXT.format(first_name,BOT_NAME,uptime),
+    reply_markup=InlineKeyboardMarkup(buttons))
