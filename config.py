89bd32fe5bb9e6a4040c1dbb146d23dd4105b3c8
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-22 15:12:40 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,12 +2,21 @@ class Config (object):
     LOG=True
     LOAD=""
     NO_LOAD="translation"
+    URL=None
+    WORKERS = 8   
+    PORT = 5000 
 
     API_ID=16191628
     API_HASH="7d5acccaf1df4f5b7a690b203fd1953e"
     BOT_TOKEN="5918324070:AAHvKsRll72lGTwgPzQoaaEpLXlnZD71QnQ"
-    WORKERS = 8
-    URL=None
+
+    OWNER_ID = 5556308886
+    SUPPORT_CHAT = "StarkSupport_21"
+
+    BOT_NAME = "• ғ ʀ ɪ ᴅ ᴀ ʏ •"
+    BOT_USERNAME = "Friday_Ro_Bot"
+    
+
 
 
 class Friday (Config):
