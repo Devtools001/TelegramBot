d7c9e148b479701ad272dd657df9c52b777374cf
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-24 23:06:19 +0530

    Update tagall.py

--- TeleBot/modules/tagall.py
+++ TeleBot/modules/tagall.py
@@ -9,6 +9,6 @@ async def tag_all(_,msg):
     async for m in pgram.get_chat_members(msg.chat.id):
         usernme+=1
         usertext+= f"[{m.user.first_name}](tg://user?id={m.user.id})"
-        if username==5:
+        if usernme==5:
             text=f"hii \n{usrtxt}"
             await pgram.send_message(msg.chat.id,text)
