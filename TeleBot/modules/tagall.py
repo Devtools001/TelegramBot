52b68101a33c164c754a5f20ff2a311d86ff42df
StarkBotsTesting <118409000+StarkBotsTesting@users.noreply.github.com>
2022-11-24 22:38:36 +0530

    Create tagall.py

---
+++ TeleBot/modules/tagall.py
@@ -0,0 +1,8 @@
+from TeleBot import pgram
+from pyrogram import filters
+
+
+@app.on_message(filters.command("tagall"))
+async def tag_all(_,msg):
+    async for m in pgram.get_chat_members(msg.chat.id):
+        await msg.reply_text(f"hey {m.user.mention}")
