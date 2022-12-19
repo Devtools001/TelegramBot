b9a3756b8dbc8fb545efeec53e7ef771b0816f46
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-19 21:36:57 +0530

    Update __main__.py

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -478,7 +478,7 @@ def main():
         LOG.info("Using long polling.")
         updater.start_polling(timeout=15, read_latency=4, drop_pending_updates=True)
    
-     if len(argv) not in (1, 3, 4):
+    if len(argv) not in (1, 3, 4):
         telethn.disconnect()
     else:
         telethn.run_until_disconnected()
