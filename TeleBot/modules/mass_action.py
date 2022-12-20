552afe2258eda4707025d97ea58c58f1eb0b6142
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-20 23:43:30 +0530

    Update mass_action.py

--- TeleBot/modules/mass_action.py
+++ TeleBot/modules/mass_action.py
@@ -40,7 +40,10 @@ def bot_admin(stark):
         user = await app.get_chat_member(chat_id, message.from_user.id)
 
         if user.status == ChatMemberStatus.MEMBER:
-            pass 
+            try:
+                pass:
+            except Exception:
+                pass 
       
         else:
             if bot.status != ChatMemberStatus.ADMINISTRATOR:
