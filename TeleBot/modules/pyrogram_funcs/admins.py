d5243d8ac575186271bf02ce2ec3898d4c705705
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-20 13:46:15 +0530

    Update and rename admins.txt to admins.py

---
+++ TeleBot/modules/pyrogram_funcs/admins.py
@@ -0,0 +1,36 @@
+from functools import wraps
+from TeleBot import pgram
+from threading import RLock
+from cachetools import TTLCache
+from pyrogram.types import ChatMember
+
+ADMIN_CACHE = TTLCache(maxsize=512, ttl=60 * 10, timer=perf_counter)
+THREAD_LOCK = RLock()
+        
+def is_user_admin(_, msg) -> bool:
+    admin = "administrator"
+    creator = "creator"
+    ranks = [admin, creator]
+    chat_id = msg.chat.id
+    user_id = msg.from_user.id
+    member = await app.get_chat_member(chat_id,user_id)
+    administrators = []
+    async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
+        administrators.append(m.user.id)
+    if (
+        msg.chat.type == "private"       
+        or user_id in administrators
+        or user_id in {1087968824}
+    ): 
+        return True
+    if member.status in ranks:
+        return True
+                      
+    with THREAD_LOCK:        
+        try:
+            return user_id in ADMIN_CACHE[chat_id]
+        except KeyError:
+            pass 
+           
+            
+            
