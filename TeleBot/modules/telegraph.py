e6277db49ec580f2f4c68de80cf51303e7a46e00
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-21 10:40:30 +0530

    Update telegraph.py

--- TeleBot/modules/telegraph.py
+++ TeleBot/modules/telegraph.py
@@ -34,7 +34,7 @@ async def telegraph(app, message):
             InlineKeyboardButton(text="Open Link", url=f"https://graph.org{downloaded_file[0]}"),
             InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{downloaded_file[0]}")
             ],[
-            InlineKeyboardButton(text="✗ Close ✗", url="ok")
+            InlineKeyboardButton(text="✗ Close ✗", url="https://t.me/HokagexRobot")
             ]]
         )
     )  
