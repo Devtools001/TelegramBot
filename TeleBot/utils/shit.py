d3d906fd20f5c20fbb009cbfa8c8fd5facc87ea6
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-21 11:45:05 +0530

    Create shit.py

---
+++ TeleBot/utils/shit.py
@@ -0,0 +1,11 @@
+
+
+TELEGRAPH = InlineKeyboardMarkup( [[
+            InlineKeyboardButton(text="Open Link", url=f"https://graph.org{downloaded_file[0]}"),
+            InlineKeyboardButton(text="Share Link", url=f"https://telegram.me/share/url?url=https://graph.org{downloaded_file[0]}")
+            ],[
+            InlineKeyboardButton(text="✗ Close ✗", callback_data="close")
+            ]]
+        )
+    )  
+        
