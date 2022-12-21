eff22aba86a39a0b2798ef49bc8ef4b76b5b75ea
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-21 12:08:27 +0530

    Update telegraph.py

--- TeleBot/modules/telegraph.py
+++ TeleBot/modules/telegraph.py
@@ -11,26 +11,27 @@ telegraph.create_account(short_name=BOT_NAME)
 @pgram.on_message(filters.command(["tgm","tgt"]))
 async def telegraph(app, message):
     replied = message.reply_to_message
-    if not replied:
-        await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ɢᴇᴛ ᴀ ᴘᴇʀᴍᴀɴᴇɴᴛ telegra.ph link")
-        return 
+    if message.command == "tgm":
+        if not replied:
+            await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇꜱꜱᴀɢᴇ ᴛᴏ ɢᴇᴛ ᴀ ᴘᴇʀᴍᴀɴᴇɴᴛ telegra.ph link")
+            return 
     
-    elif replied.media or replied.text:
-        text = await message.reply("Downloading to My Server")
-        media = await replied.download()
-        await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)
-        try:
-            downloaded_file = upload_file(media)
-        except Exception as error:
-            print(error)
-            await text.edit(text=f"Error :- {error}", disable_web_page_preview=True)       
-            return    
-        try:
-            os.remove(media)
-        except Exception as error:
-            print(error)
-            return  
-        await text.edit(
+        elif replied.media or replied.text:
+            text = await message.reply("Downloading to My Server")
+            media = await replied.download()
+            await text.edit_text(text="<code>Downloading Completed. Now I am Uploading to telegra.ph Link ...</code>", disable_web_page_preview=True)
+            try:
+                downloaded_file = upload_file(media)
+            except Exception as error:
+                print(error)
+                await text.edit(text=f"Error :- {error}", disable_web_page_preview=True)       
+                return    
+            try:
+                os.remove(media)
+            except Exception as error:
+                print(error)
+                return  
+            await text.edit(
         text=f"<b>Link :-</b>\n\n<code>https://graph.org{downloaded_file[0]}</code>",
         disable_web_page_preview=False,
         reply_markup=InlineKeyboardMarkup( [
