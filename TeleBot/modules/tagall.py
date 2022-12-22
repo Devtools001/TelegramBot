ba63fc572f05a9b93420caee34219d93102a651f
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 13:48:57 +0530

    Update and rename tagall.txt to tagall.py

---
+++ TeleBot/modules/tagall.py
@@ -0,0 +1,22 @@
+from TeleBot import pgram
+from pyrogram import filters
+
+
+@pgram.on_message(filters.command("tagall"))
+async def tag_all(_,msg):
+    members = []
+    usernme=0
+    logo_text = (
+            message.text.split(None, 1)[1]
+            if len(message.command) < 3
+    async for m in pgram.get_chat_members(msg.chat.id):
+        members.append(m.user.mention) 
+        print(members)   
+
+    
+    usernme += 1
+        usertext += m.user.mention
+        if usernme==5:
+            text=f"hii \nlogo_text"
+            await pgram.send_message(msg.chat.id,text)
+        
