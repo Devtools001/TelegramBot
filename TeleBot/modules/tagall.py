e822958bf3e7063cc4fb1dab2348802df9d5823a
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 14:45:47 +0530

    Update tagall.py

--- TeleBot/modules/tagall.py
+++ TeleBot/modules/tagall.py
@@ -14,7 +14,7 @@ async def tag_all(_,message):
     username=0
     usertext = ''
     async for m in pgram.get_chat_members(message.chat.id):
-        if not chat_id in spam_chats:
+        if not message.chat.id in spam_chats:
             break
         username += 1
         usertext += f"\n[{m.user.first_name}](tg://user?id={m.user.id})"
