0e922e38898ef9a325bc51d9177b04da1592c9c7
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 21:26:27 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -2,18 +2,9 @@ from TeleBot import pgram
 from pyrogram import filters
 
 from pyrogram.handlers import MessageHandler
+async def my_handler(_, message)
+r = await pgram.get_chat(-1001698076323)
+    print(str(r))
 
-#async def my_handler(client, message):
-#    if message.reply_to_message.sticker:
- #       await pgram.send_message(message.chat.id,"hii how are u")
-#        print(message)
+pgram.add_handler(MessageHandler(my_handler, filters.command("okk"))    
 
-#pgram.add_handler(MessageHandler(my_handler, filters.command("okk") & filters.sticker))    
-
-@pgram.on_message(filters.text | filters.sticker)
-async def text_or_sticker(client, message):
-    print("text_or_sticker")
-
-@pgram.on_message(filters.text, group=-1)
-async def just_text(client, message):
-    print("Just Text")
