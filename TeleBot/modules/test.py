485d6c5df7ff30a7bd3a1cd6c20511a6919e6673
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 20:54:03 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 
 from pyrogram.handlers import MessageHandler
 async def my_handler(client, message):
-        await message.reply_text("hii bro")
+        await pgram.send_message(message.chat.id,"hii how are u")
         print(message)
 
 pgram.add_handler(MessageHandler(my_handler, filters.command("okk")))    
