a54d06cc3da4a5de1ba1499a9be83c1a3c64ef63
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 12:21:07 +0530

    Update logo.py

--- TeleBot/modules/logo.py
+++ TeleBot/modules/logo.py
@@ -62,9 +62,9 @@ async def logo_make(_,message):
    
     if replied:
         if replied.photo:
-            photo = await pgram.download_media(replied.photo.file_id,in_memory=True)
+            file = await pgram.download_media(replied.photo.file_id,in_memory=True)
             file_name = file.name
             file_bytes = bytes(file.getbuffer())
             print(file_bytes)
-            os.remove(photo)
+            os.remove(file)
            
