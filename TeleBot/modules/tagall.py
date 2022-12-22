aaba5af076b50c5f4954449abc8984a3a2a4cb72
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 14:36:18 +0530

    Update tagall.py

--- TeleBot/modules/tagall.py
+++ TeleBot/modules/tagall.py
@@ -3,20 +3,25 @@ from TeleBot import pgram
 from pyrogram import filters
 
 
-#@pgram.on_message(filters.command("tagall"))
-#async def tag_all(_,message):  
+@pgram.on_message(filters.command("tagall"))
+async def tag_all(_,message):  
  
-#    logo_text = (
-#            message.text.split(None, 1)[1]
-#            if len(message.command) < 3 else None)
-#    username=0
-#    usertext = ''
-#   async for m in pgram.get_chat_members(message.chat.id):
-#        username += 1
- #       usertext += f"[{m.user.first_name}](tg://user?id={m.user.id}) "
- #       if username == 5:
-#            await pgram.send_message(message.chat.id,f"{usertext}\n\n{logo_text}")
-#            await asyncio.sleep(1)
+    logo_text = (
+            message.text.split(None, 1)[1]
+            if len(message.command) < 3 else None)
+    username=0
+    usertext = ''
+    async for m in pgram.get_chat_members(message.chat.id):
+        username += 1
+        usertext += f"\n[{m.user.first_name}](tg://user?id={m.user.id})"
+    async for i in range(1000000000):
+        if username == 5
+        await pgram.send_message(message.chat.id,f"{usertext}\n\n{logo_text}")
+        await asyncio.sleep(0.5)
+
+   
+        
+           
 
 @pgram.on_message(filters.command("tag"))
 async def tag_all(_,message):       
