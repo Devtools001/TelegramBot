4c7290d93677f18b1f1cfaddf2bb1c1aca7fcdd3
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 14:37:06 +0530

    Update tagall.py

--- TeleBot/modules/tagall.py
+++ TeleBot/modules/tagall.py
@@ -15,7 +15,7 @@ async def tag_all(_,message):
         username += 1
         usertext += f"\n[{m.user.first_name}](tg://user?id={m.user.id})"
     async for i in range(1000000000):
-        if username == 5
+        if username == 5:
         await pgram.send_message(message.chat.id,f"{usertext}\n\n{logo_text}")
         await asyncio.sleep(0.5)
 
