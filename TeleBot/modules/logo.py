6a23473579079caf8ebd2474bd6a8dbe3b92244f
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 16:18:51 +0530

    Update logo.py

--- TeleBot/modules/logo.py
+++ TeleBot/modules/logo.py
@@ -56,28 +56,11 @@ async def logo_make(_,message):
     if not replied:
         try:
             randc = random.choice(LOGOES)
-            logo = Image.open(io.BytesIO(requests.get(randc).content))            
-            draw = ImageDraw.Draw(logo) 
-            image_widthz, image_heightz = logo.size
-            pointsize = 500
-            fillcolor = "black"
-            shadowcolor = "blue"
-            fnt = glob.glob("./TeleBot/resources/Logo_fonts/*")
-            randf = random.choice(fnt)
-            font = ImageFont.truetype(randf, 120)
-            w, h = draw.textsize(logo_text, font=font)
-            h += int(h*0.21)
-            image_width, image_height = logo.size
-            draw.text(((image_widthz-w)/2, (image_heightz-h)/2), logo_text, font=font, fill=(255, 255, 255))
-            x = (image_widthz-w)/2
-            y = ((image_heightz-h)/2+6)
-            draw.text((x, y), logo_text, font=font, fill="white", stroke_width=1, stroke_fill="black")
-            final_logo = "friday.png"
-            logo.save(final_logo, "png")
+            final = await logo_vai(link=randc,logo_text=logo_text)
             await pgram.send_photo(chat_id,final_logo)
             await text.delete()
-            if os.path.exists(final_logo):
-                os.remove(final_logo)                
+            if os.path.exists(final):
+                os.remove(final)                
         except Exception as e:
             await message.reply_text(e)
     
