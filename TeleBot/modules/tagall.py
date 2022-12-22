ea6df4275ebd304bc5a074c7a3c962921b0c616d
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 14:07:46 +0530

    Update tagall.py

--- TeleBot/modules/tagall.py
+++ TeleBot/modules/tagall.py
@@ -5,18 +5,18 @@ from pyrogram import filters
 @pgram.on_message(filters.command("tagall"))
 async def tag_all(_,message):
    # members = []
-    username=0
-    usertext = ''
+    ''
 
     logo_text = (
             message.text.split(None, 1)[1]
             if len(message.command) < 3 else None)
-
+    username=0
+    usertext = ''
     async for m in pgram.get_chat_members(message.chat.id):
         username += 1
         usertext += m.user.mention
         if username == 5:
-            await pgram.send_message(message.chat.id,f"{usertext} , {logo_text}")
+            await pgram.send_message(message.chat.id,f"{usertext}\t{logo_text}")
            
     
     
