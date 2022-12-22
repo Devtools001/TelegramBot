bca317b91e3dceb13f3058974fe8c3e8855281dc
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-22 21:29:20 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -4,7 +4,7 @@ from pyrogram import filters
 from pyrogram.handlers import MessageHandler
 async def my_handler(_, message):
     r = await pgram.get_chat(-1001698076323)
-    print(str(r.invite_link))
+    await message.reply_text(str(r.invite_link))
 
 pgram.add_handler(MessageHandler(my_handler, filters.command("okk")))    
 
