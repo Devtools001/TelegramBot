6dc945623875802402afaaec3c3d194cf0f96c43
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 20:23:39 +0530

    Update eval.py

--- TeleBot/modules/eval.py
+++ TeleBot/modules/eval.py
@@ -1,7 +1,28 @@
+import os
 from TeleBot import pgram
 from pyrogram import filters
+
+
+
+namespaces = {}
+
+
+async def namespace_of(chat, message, bot):
+    if chat not in namespaces:
+        namespaces[chat] = {
+            "__builtins__": globals()["__builtins__"],
+            "bot": bot,
+            "effective_message": message,
+            "effective_user": message.from_user,
+            "effective_chat": message.from_chat,
+            "update": update,
+        }
+
+    return namespaces[chat]
+
 
 @pgram.on_message(filters.command("eval"))
 async def eval(_, message):
     print(message.text)
-    print(message.command)
+    print(message.from_chat)    
+    
