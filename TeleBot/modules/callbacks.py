6410f3c0041ae1f668a5bea4dae82725225c3d5a
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 14:51:13 +0530

    Update callbacks.py

--- TeleBot/modules/callbacks.py
+++ TeleBot/modules/callbacks.py
@@ -3,33 +3,7 @@ import psutil
 from pyrogram import filters , __version__ as pyro , Client 
 from TeleBot import pgram,StartTime,BOT_NAME,get_readable_time,BOT_USERNAME
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery 
-
-
-PM_START_TEXT = """
-🥀 ʜᴇʏ **{}** ,
-        
-ᴛʜɪs ɪs **{}**
-➖➖➖➖➖➖➖➖➖➖➖➖➖
-
-๏ ɪ'ᴍ ᴜɴᴅᴇʀ ᴄʀᴇᴀᴛɪᴏɴ ʙʏ ⸢[𝚂𝚃𝙰𝚁𝙺](https://t.me/NoobStark_21)⸥
-๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅsn.
-
-๏ **ᴅɪᴅɴ'ᴛ sʟᴇᴇᴘᴛ sɪɴᴄᴇ** {}
-
-"""
-
-BUTTON = [
-    [
-        InlineKeyboardButton(
-            text="✨ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨",
-            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-        ),
-    ],    
-    [
-        InlineKeyboardButton(text="⚡ ʜᴇʟᴘ ⚡", callback_data="help_back"),
-        InlineKeyboardButton(text="♻️ sᴛᴀᴛs ♻️", callback_data="Friday_stats")
-    ],    
-]
+from TeleBot.resources.Data import *
 
 STATS_MSG="""
 ʜɪɪ {},
@@ -47,7 +21,7 @@ async def Friday(_, callback_query : CallbackQuery):
     await query.delete()
     first_name=callback_query.from_user.first_name
     uptime= get_readable_time((time.time() - StartTime))
-    await query.reply_text(PM_START_TEXT.format(first_name,BOT_NAME,uptime),
+    await query.reply_photo(photo=random.choice(PM_PHOTOS),caption=PM_START_TEXT.format(first_name,BOT_NAME,uptime),
     reply_markup=InlineKeyboardMarkup(BUTTON))
 
 @pgram.on_callback_query(filters.regex("Friday_stats"))
