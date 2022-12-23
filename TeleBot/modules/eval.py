9c9891ccd26c533b90be0de43df79ded98a5bf83
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 20:16:04 +0530

    Update eval.py

--- TeleBot/modules/eval.py
+++ TeleBot/modules/eval.py
@@ -2,6 +2,6 @@ from TeleBot import pgram
 from pyrogram import filters
 
 @pgram.on_message(filters.command("eval"))
-async def(_, message):
+async def eval(_, message):
     print(message.text)
     print(message.command)
