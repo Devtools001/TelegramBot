fa020d30c6dbfb10890c50327f1fe2a71f832582
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 12:28:05 +0530

    Update __main__.py

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -1,76 +1,32 @@
-import re
+
 import asyncio
 import importlib
+import re
 from contextlib import closing, suppress
+
 from uvloop import install
+from pyrogram import filters, idle
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from TeleBot import (
     BOT_NAME,
     BOT_USERNAME,        
     aiohttpsession,
-    pgram,
+    pgram as app,
     LOG,
-    get_readable_time,
 )
-from pyrogram import filters,Client
-from pyrogram.types import Message
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from pyrogram.handlers import MessageHandler
 from TeleBot.modules import ALL_MODULES
 from TeleBot.utilities.misc import paginate_modules
 from TeleBot.utilities.constant import MARKDOWN
 
-
-#loop = asyncio.get_event_loop()
-
-START_IMG="https://telegra.ph/file/5381961c760ed435d0fc7.jpg"
-
-PM_PHOTOS = (
-"https://telegra.ph/file/9658f5c0f7d448ad4e0bd.jpg",
-"https://telegra.ph/file/4a94521e20c40195c9a9a.jpg",
-"https://telegra.ph/file/61c9b7921458f31260b0c.jpg",
-"https://telegra.ph/file/9e01586796e950cc8ddc6.jpg",
-"https://telegra.ph/file/0e068d1357c5a79c191e3.jpg",
-"https://telegra.ph/file/13b5bc8b84ee2ac687e3d.jpg",
-"https://telegra.ph/file/7517a65cab490e36d681c.jpg"
-         )
-
-HELP_IMG=(
-"https://telegra.ph/file/74f547d4dd635432ea1b0.jpg",
-"https://telegra.ph/file/b846cc2a4326a9dab5a2d.jpg",
-"https://telegra.ph/file/1c58b1a72044ba6b6e644.jpg"
-   )
-
-
-PM_START_TEXT = """
-🥀 ʜᴇʏ *{}* ,
-        
-ᴛʜɪs ɪs *{}*
-➖➖➖➖➖➖➖➖➖➖➖➖➖
-๏ ɪ'ᴍ ᴜɴᴅᴇʀ ᴄʀᴇᴀᴛɪᴏɴ ʙʏ ⸢[𝚂𝚃𝙰𝚁𝙺](https://t.me/NoobStark_21)⸥
-๏ ᴄʟɪᴄᴋ ᴏɴ ᴛʜᴇ ʜᴇʟᴩ ʙᴜᴛᴛᴏɴ ᴛᴏ ɢᴇᴛ ɪɴғᴏʀᴍᴀᴛɪᴏɴ ᴀʙᴏᴜᴛ ᴍʏ ᴍᴏᴅᴜʟᴇs ᴀɴᴅ ᴄᴏᴍᴍᴀɴᴅsn.
-๏ *ᴅɪᴅɴ'ᴛ sʟᴇᴇᴘᴛ sɪɴᴄᴇ* {}
-"""
-
-buttons = [
-    [
-        InlineKeyboardButton(
-            text="✨ᴀᴅᴅ ᴍᴇ ʙᴀʙʏ ✨",
-            url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
-        ),
-    ],    
-    [
-        InlineKeyboardButton(text="⚡ ʜᴇʟᴘ ⚡", callback_data="help_back"),
-        InlineKeyboardButton(text="♻️ sᴛᴀᴛs ♻️", callback_data="Friday_stats")
-    ],    
-]
-
-
-
+loop = asyncio.get_event_loop()
 
 HELPABLE = {}
 
-async def bot():
+
+async def start_bot():
+    global HELPABLE
+
     for module in ALL_MODULES:
         imported_module = importlib.import_module("TeleBot.modules." + module)
         if (
@@ -85,35 +41,303 @@ async def bot():
                 HELPABLE[
                     imported_module.__mod_name__.replace(" ", "_").lower()
                 ] = imported_module
-
-
-
-
-#async def send_help(chat_id, text, keyboard=None):
-#    if not keyboard:
-#        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
-#    dispatcher.bot.send_photo(
-#        chat_id=chat_id,
-#        photo=random.choice(PM_PHOTOS),
-#        caption=text,
-#        parse_mode=ParseMode.MARKDOWN,      
-#        reply_markup=keyboard,
-#    )
-
-async def start(client : Client, message: Message):
-    await message.reply_text("hii")
-
-pgram.add_handler(MessageHandler(start, filters.command("start")))
-
-
-@pgram.on_message(filters.command("start"))
-async def start(_, message):
-    await message.reply_text("hii") 
-
-
-
-if __name__ == "__main__" :
-    LOG.print("[yellow] bot started")
-        
+    bot_modules = ""
+    j = 1
+    for i in ALL_MODULES:
+        if j == 4:
+            bot_modules += "|{:<15}|\n".format(i)
+            j = 0
+        else:
+            bot_modules += "|{:<15}".format(i)
+        j += 1
+    print("+===============================================================+")
+    print("|                              WBB                              |")
+    print("+===============+===============+===============+===============+")
+    print(bot_modules)
+    print("+===============+===============+===============+===============+")
+    LOG.print(f"[bold red]BOT STARTED AS {BOT_NAME}!")
     
+    
+    try:
+        LOG.print("[yellow]Sending online status")              
+        await app.send_message(-1001698076323, "Bot started!")
+    except Exception:
+        pass
 
+    await idle()
+
+    await aiohttpsession.close()   
+    LOG.print("[yello] stopping client") 
+    await app.stop()
+    LOG.print("[yellow]Cancelling asyncio tasks")
+    for task in asyncio.all_tasks():
+        task.cancel()
+    LOG.print("[yello]Dead!")
+
+
+home_keyboard_pm = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="Commands ❓", callback_data="bot_commands"
+            ),
+            InlineKeyboardButton(
+                text="Repo 🛠",
+                url="https://github.com/thehamkercat/WilliamButcherBot",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="System Stats 🖥",
+                callback_data="stats_callback",
+            ),
+            InlineKeyboardButton(
+                text="Support 👨", url="http://t.me/WBBSupport"
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="Add Me To Your Group 🎉",
+                url=f"http://t.me/{BOT_USERNAME}?startgroup=new",
+            )
+        ],
+    ]
+)
+
+home_text_pm = (
+        f"Hey there! My name is {BOT_NAME}. I can manage your "
+        + "group with lots of useful features, feel free to "
+        + "add me to your group."
+)
+
+keyboard = InlineKeyboardMarkup(
+    [
+        [
+            InlineKeyboardButton(
+                text="Help ❓",
+                url=f"t.me/{BOT_USERNAME}?start=help",
+            ),
+            InlineKeyboardButton(
+                text="Repo 🛠",
+                url="https://github.com/thehamkercat/WilliamButcherBot",
+            ),
+        ],
+        [
+            InlineKeyboardButton(
+                text="System Stats 💻",
+                callback_data="stats_callback",
+            ),
+            InlineKeyboardButton(text="Support 👨", url="t.me/WBBSupport"),
+        ],
+    ]
+)
+
+
+@app.on_message(filters.command("start"))
+async def start(_, message):
+    if message.chat.type != "private":
+        return await message.reply(
+            "Pm Me For More Details.", reply_markup=keyboard
+        )
+    if len(message.text.split()) > 1:
+        name = (message.text.split(None, 1)[1]).lower()
+        if name == "mkdwn_help":
+            await message.reply(
+                MARKDOWN, parse_mode="html", disable_web_page_preview=True
+            )
+        elif "_" in name:
+            module = name.split("_", 1)[1]
+            text = (
+                    f"Here is the help for **{HELPABLE[module].__mod_name__}**:\n"
+                    + HELPABLE[module].__help__
+            )
+            await message.reply(text, disable_web_page_preview=True)
+        elif name == "help":
+            text, keyb = await help_parser(message.from_user.first_name)
+            await message.reply(
+                text,
+                reply_markup=keyb,
+            )
+    else:
+        await message.reply(
+            home_text_pm,
+            reply_markup=home_keyboard_pm,
+        )
+    return
+
+
+
+@app.on_message(filters.command("help"))
+async def help_command(_, message):
+    if message.chat.type != "private":
+        if len(message.command) >= 2:
+            name = (message.text.split(None, 1)[1]).replace(" ", "_").lower()
+            if str(name) in HELPABLE:
+                key = InlineKeyboardMarkup(
+                    [
+                        [
+                            InlineKeyboardButton(
+                                text="Click here",
+                                url=f"t.me/{BOT_USERNAME}?start=help",
+                            )
+                        ],
+                    ]
+                )
+                await message.reply(
+                    f"Click on the below button to get help about {name}",
+                    reply_markup=key,
+                )
+            else:
+                await message.reply(
+                    "PM Me For More Details.", reply_markup=keyboard
+                )
+        else:
+            await message.reply(
+                "Pm Me For More Details.", reply_markup=keyboard
+            )
+    else:
+        if len(message.command) >= 2:
+            name = (message.text.split(None, 1)[1]).replace(" ", "_").lower()
+            if str(name) in HELPABLE:
+                text = (
+                        f"Here is the help for **{HELPABLE[name].__mod_name__}**:\n"
+                        + HELPABLE[name].__help__
+                )
+                await message.reply(text, disable_web_page_preview=True)
+            else:
+                text, help_keyboard = await help_parser(
+                    message.from_user.first_name
+                )
+                await message.reply(
+                    text,
+                    reply_markup=help_keyboard,
+                    disable_web_page_preview=True,
+                )
+        else:
+            text, help_keyboard = await help_parser(
+                message.from_user.first_name
+            )
+            await message.reply(
+                text, reply_markup=help_keyboard, disable_web_page_preview=True
+            )
+    return
+
+
+async def help_parser(name, keyboard=None):
+    if not keyboard:
+        keyboard = InlineKeyboardMarkup(paginate_modules(0, HELPABLE, "help"))
+    return (
+        """Hello {first_name}, My name is {bot_name}.
+I'm a group management bot with some useful features.
+You can choose an option below, by clicking a button.
+Also you can ask anything in Support Group.
+""".format(
+            first_name=name,
+            bot_name=BOT_NAME,
+        ),
+        keyboard,
+    )
+
+
+@app.on_callback_query(filters.regex("bot_commands"))
+async def commands_callbacc(_, CallbackQuery):
+    text, keyboard = await help_parser(CallbackQuery.from_user.mention)
+    await app.send_message(
+        CallbackQuery.message.chat.id,
+        text=text,
+        reply_markup=keyboard,
+    )
+
+    await CallbackQuery.message.delete()
+
+
+
+
+
+@app.on_callback_query(filters.regex(r"help_(.*?)"))
+async def help_button(client, query):
+    home_match = re.match(r"help_home\((.+?)\)", query.data)
+    mod_match = re.match(r"help_module\((.+?)\)", query.data)
+    prev_match = re.match(r"help_prev\((.+?)\)", query.data)
+    next_match = re.match(r"help_next\((.+?)\)", query.data)
+    back_match = re.match(r"help_back", query.data)
+    create_match = re.match(r"help_create", query.data)
+    top_text = f"""
+Hello {query.from_user.first_name}, My name is {BOT_NAME}.
+I'm a group management bot with some usefule features.
+You can choose an option below, by clicking a button.
+Also you can ask anything in Support Group.
+
+General command are:
+ - /start: Start the bot
+ - /help: Give this message
+ """
+    if mod_match:
+        module = (mod_match.group(1)).replace(" ", "_")
+        text = (
+                "{} **{}**:\n".format(
+                    "Here is the help for", HELPABLE[module].__mod_name__
+                )
+                + HELPABLE[module].__help__
+        )
+
+        await query.message.edit(
+            text=text,
+            reply_markup=InlineKeyboardMarkup(
+                [[InlineKeyboardButton("back", callback_data="help_back")]]
+            ),
+            disable_web_page_preview=True,
+        )
+    elif home_match:
+        await app.send_message(
+            query.from_user.id,
+            text=home_text_pm,
+            reply_markup=home_keyboard_pm,
+        )
+        await query.message.delete()
+    elif prev_match:
+        curr_page = int(prev_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(curr_page - 1, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif next_match:
+        next_page = int(next_match.group(1))
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(next_page + 1, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif back_match:
+        await query.message.edit(
+            text=top_text,
+            reply_markup=InlineKeyboardMarkup(
+                paginate_modules(0, HELPABLE, "help")
+            ),
+            disable_web_page_preview=True,
+        )
+
+    elif create_match:
+        text, keyboard = await help_parser(query)
+        await query.message.edit(
+            text=text,
+            reply_markup=keyboard,
+            disable_web_page_preview=True,
+        )
+
+    return await client.answer_callback_query(query.id)
+
+
+if __name__ == "__main__":
+    install()
+    loop.run_until_complete(start_bot())
+        
+            
+        
