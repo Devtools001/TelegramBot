1f4f653db1bb0e2649f9accacd9f979c967e0784
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 14:25:22 +0530

    Update __main__.py

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -279,13 +279,13 @@ async def help_command(_, message):
                     reply_markup=help_keyboard,
                     disable_web_page_preview=True,
                 )
-        else:
-            text, help_keyboard = await help_parser(
-                message.from_user.first_name
+    else:
+        text, help_keyboard = await help_parser(
+        message.from_user.first_name
             )
-            await message.reply(
+        await message.reply(
                 text, reply_markup=help_keyboard, disable_web_page_preview=True
-            )
+            )        
     return        
     
 
