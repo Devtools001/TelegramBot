344bfe7a01e441a4e7bb17648853444a7b4afd61
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-23 14:41:04 +0530

    Update callbacks.py

--- TeleBot/modules/callbacks.py
+++ TeleBot/modules/callbacks.py
@@ -43,10 +43,11 @@ STATS_MSG="""
 
 @pgram.on_callback_query(filters.regex("friday_back"))
 async def Friday(_, callback_query : CallbackQuery):
+    await query.message.delete()
     query= callback_query.message
     first_name=callback_query.from_user.first_name
     uptime= get_readable_time((time.time() - StartTime))
-    await query.edit_caption(PM_START_TEXT.format(first_name,BOT_NAME,uptime),
+    await query.reply_text(PM_START_TEXT.format(first_name,BOT_NAME,uptime),
     reply_markup=InlineKeyboardMarkup(BUTTON))
 
 @pgram.on_callback_query(filters.regex("Friday_stats"))
