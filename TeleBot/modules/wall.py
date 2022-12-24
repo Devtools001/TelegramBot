e6e3b384a667491cbad2cf7e157d4c73b4876173
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-24 09:26:14 +0530

    Update wall.py

--- TeleBot/modules/wall.py
+++ TeleBot/modules/wall.py
@@ -5,9 +5,29 @@ from random import randint
 
 @pgram.on_message(filters.command("wal"))
 async def wallpaper (_,msg):
-    url=f"https://wallhaven.cc/api/v1/search?q=Naruto"
+    if len(msg.command) < 2:
+         await msg.reply_text("ʜᴇʏ ɴᴏᴏʙ ɢɪᴠᴇ sᴏᴍᴇᴛʜɪɴɢ ᴛᴏ sᴇᴀʀᴄʜ.")
+         return 
+    else:
+         pass
+
+    query=(
+       msg.text.split(None,1)[1]
+       if len(msg.command) < 3
+       else msg.text.split(None,1)[1].replace(" ","%20")
+       )
+    
+    if not query:
+        await msg.reply_text("ʜᴇʏ ɴᴏᴏʙ ɢɪᴠᴇ sᴏᴍᴇᴛʜɪɴɢ ᴛᴏ sᴇᴀʀᴄʜ.")
+    else:
+        pass  
+    url=f"https://wallhaven.cc/api/v1/search?q={query}"
     re=requests.get(url).json()
-    print(re)
+    walls = re.get("data")
+    wall_index = randint(0, len(walls) -1)
+    wallpaper = walls[wall_index]
+    print(wallpaper)
+    
 
 __help__ = "hii"
 __mod_name__ = "sup"
