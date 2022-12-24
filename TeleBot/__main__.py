2da0dc1711c9f5bf2ab65dcf85421567b56f2794
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-24 11:24:09 +0530

    Update __main__.py

--- TeleBot/__main__.py
+++ TeleBot/__main__.py
@@ -2,9 +2,13 @@ import asyncio
 import os
 import re
 import time
+import random 
+import importlib
 from datetime import datetime
 from typing import Optional
 from sys import argv
+from uvloop import install
+from contextlib import closing, suppress
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 
 from pyrogram.errors import BadRequest 
@@ -12,6 +16,9 @@ from pyrogram.types import CallbackQuery
 from pyrogram import filters,idle
 
 from TeleBot.modules import ALL_MODULES
-
 from TeleBot.modules.utilities.misc import paginate_modules
+
+
  
+
+
