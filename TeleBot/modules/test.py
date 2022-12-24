7e7d786df76a2043f7876b96d4b5e306b5387c33
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-24 18:22:11 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -6,7 +6,7 @@ from pyrogram.handlers import MessageHandler
 @app.on_messsge(filters.command("hii"))
 async def hii(_,msg):
     print(msg.sender_chat)
-    if msg.chat.type ! = ChatType.PRIVATE:
+    if msg.chat.type != ChatType.PRIVATE:
         await msg.reply_text("public h")
     else:
         await msg.reply_text("private h")
