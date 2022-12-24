49a358b967271e8c5bc9b52216ac6fd51501c57c
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-24 11:01:18 +0530

    Update misc.py

--- TeleBot/utilities/misc.py
+++ TeleBot/utilities/misc.py
@@ -5,6 +5,7 @@ from pyrogram.types import InlineKeyboardButton
 from TeleBot import MOD_LOAD, MOD_NOLOAD
 
 
+
 class EqInlineKeyboardButton(InlineKeyboardButton):
     def __eq__(self, other):
         return self.text == other.text
@@ -16,14 +17,15 @@ class EqInlineKeyboardButton(InlineKeyboardButton):
         return self.text > other.text
 
 
-def paginate_modules(page_n, module_dict, prefix, chat=None):
+
+def paginate_modules(page_n: int, module_dict: Dict, prefix, chat=None) -> List:
     if not chat:
         modules = sorted(
             [
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({})".format(
-                        prefix, x.__mod_name__.replace(" ", "_").lower()
+                        prefix, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
@@ -35,58 +37,41 @@ def paginate_modules(page_n, module_dict, prefix, chat=None):
                 EqInlineKeyboardButton(
                     x.__mod_name__,
                     callback_data="{}_module({},{})".format(
-                        prefix, chat, x.__mod_name__.replace(" ", "_").lower()
+                        prefix, chat, x.__mod_name__.lower()
                     ),
                 )
                 for x in module_dict.values()
             ]
         )
 
-    pairs = list(zip(modules[::3], modules[1::3], modules[2::3]))
-    i = 0
-    for m in pairs:
-        for _ in m:
-            i += 1
-    if len(modules) - i == 1:
+    pairs = [modules[i * 3 : (i + 1) * 3] for i in range((len(modules) + 3 - 1) // 3)]
+
+    round_num = len(modules) / 3
+    calc = len(modules) - round(round_num)
+    if calc in [1, 2]:
         pairs.append((modules[-1],))
-    elif len(modules) - i == 2:
-        pairs.append(
-            (
-                modules[-2],
-                modules[-1],
-            )
-        )
 
-    COLUMN_SIZE = 4
-
-    max_num_pages = ceil(len(pairs) / COLUMN_SIZE)
+    max_num_pages = ceil(len(pairs) / 4)
     modulo_page = page_n % max_num_pages
 
     # can only have a certain amount of buttons side by side
-    if len(pairs) > COLUMN_SIZE:
-        pairs = pairs[
-                modulo_page * COLUMN_SIZE: COLUMN_SIZE * (modulo_page + 1)
-                ] + [
-                    (
-                        EqInlineKeyboardButton(
-                            "❮",
-                            callback_data="{}_prev({})".format(prefix,
-                                                               modulo_page),
-                        ),
-                        EqInlineKeyboardButton(
-                            "Back",
-                            callback_data="friday_back",
-                        ),
-                        EqInlineKeyboardButton(
-                            "❯",
-                            callback_data="{}_next({})".format(prefix,
-                                                               modulo_page),
-                        ),
-                    )
-                ]
+    if len(pairs) > 3:
+        pairs = pairs[modulo_page * 4 : 4 * (modulo_page + 1)] + [
+            (
+                EqInlineKeyboardButton(
+                    "◁", callback_data="{}_prev({})".format(prefix, modulo_page)
+                ),
+                EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="friday_back"),
+                EqInlineKeyboardButton(
+                    "▷", callback_data="{}_next({})".format(prefix, modulo_page)
+                ),
+            )
+        ]
+
+    else:
+        pairs += [[EqInlineKeyboardButton("ʙᴀᴄᴋ", callback_data="friday_back")]]
 
     return pairs
-
 
 def is_module_loaded(name):
     return (not MOD_LOAD or name in MOD_LOAD) and name not in MOD_NOLOAD
