e89e2a3533cc6bb9594ed7ca6a4c4e72f8652011
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-25 06:46:57 +0530

    Update eval.py

--- TeleBot/modules/eval.py
+++ TeleBot/modules/eval.py
@@ -12,6 +12,14 @@ from time import time
 from pyrogram.types import (InlineKeyboardButton,
                             InlineKeyboardMarkup, Message)
 
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {a}" for a in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
+
+
 async def edit_or_reply(msg: Message, **kwargs):
     func = msg.edit_text if msg.from_user.is_self else msg.reply
     spec = getfullargspec(func.__wrapped__).args
