0cdefb5ab29b9226c4a67b67bbd4053ed12ca028
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-25 06:43:05 +0530

    Update eval.py

--- TeleBot/modules/eval.py
+++ TeleBot/modules/eval.py
@@ -1,36 +1,45 @@
-import os
+
 from TeleBot import pgram as app
 from pyrogram import filters
+import os
+import re
+import subprocess
 import sys
 import traceback
+from inspect import getfullargspec
+from io import StringIO
+from time import time
 
+async def edit_or_reply(msg: Message, **kwargs):
+    func = msg.edit_text if msg.from_user.is_self else msg.reply
+    spec = getfullargspec(func.__wrapped__).args
+    await func(**{k: v for k, v in kwargs.items() if k in spec})
 
     
-@app.on_message(filters.command("eval") & filters.user(5459540851))
-async def eval(client, message):
-    status_message = await message.reply_text("Processing ...")
-    cmd = message.text.split(" ", maxsplit=1)[1]
-
-    reply_to_ = message
-    if message.reply_to_message:
-        reply_to_ = message.reply_to_message
-
+@app.on_message(filters.command("eval"))
+async def executor(client, message):
+    if len(message.command) < 2:
+        return await edit_or_reply(
+            message, text="__Nigga Give me some command to execute.__"
+        )
+    try:
+        cmd = message.text.split(" ", maxsplit=1)[1]
+    except IndexError:
+        return await message.delete()
+    t1 = time()
     old_stderr = sys.stderr
     old_stdout = sys.stdout
-    redirected_output = sys.stdout = io.StringIO()
-    redirected_error = sys.stderr = io.StringIO()
+    redirected_output = sys.stdout = StringIO()
+    redirected_error = sys.stderr = StringIO()
     stdout, stderr, exc = None, None, None
-
     try:
         await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
-
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
     evaluation = ""
     if exc:
         evaluation = exc
@@ -40,24 +49,46 @@ async def eval(client, message):
         evaluation = stdout
     else:
         evaluation = "Success"
-
-    final_output = "<b>EVAL</b>: "
-    final_output += f"<code>{cmd}</code>\n\n"
-    final_output += "<b>OUTPUT</b>:\n"
-    final_output += f"<code>{evaluation.strip()}</code> \n"
-
+    final_output = f"**OUTPUT**:\n```{evaluation.strip()}```"
     if len(final_output) > 4096:
-        with io.BytesIO(str.encode(final_output)) as out_file:
-            out_file.name = "eval.text"
-            await reply_to_.reply_document(document=out_file,
-                                           caption=cmd,
-                                           disable_notification=True)
+        filename = "output.txt"
+        with open(filename, "w+", encoding="utf8") as out_file:
+            out_file.write(str(evaluation.strip()))
+        t2 = time()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {t2-t1} Seconds",
+                    )
+                ]
+            ]
+        )
+        await message.reply_document(
+            document=filename,
+            caption=f"**INPUT:**\n`{cmd[0:980]}`\n\n**OUTPUT:**\n`Attached Document`",
+            quote=False,
+            reply_markup=keyboard,
+        )
+        await message.delete()
+        os.remove(filename)
     else:
-        await reply_to_.reply_text(final_output)
-    await status_message.delete()
-
-
-async def aexec(code, client, message):
-    exec("async def __aexec(client, message): " +
-         "".join(f"\n {l_}" for l_ in code.split("\n")))
-    return await locals()["__aexec"](client, message)
+        t2 = time()
+        keyboard = InlineKeyboardMarkup(
+            [
+                [
+                    InlineKeyboardButton(
+                        text="⏳",
+                        callback_data=f"runtime {round(t2-t1, 3)} Seconds",
+                    ),
+                    InlineKeyboardButton(
+                        text="🗑",
+                        callback_data=f"forceclose abc|{message.from_user.id}",
+                    ),
+                ]
+            ]
+        )
+        await edit_or_reply(
+            message, text=final_output, reply_markup=keyboard
+        )
