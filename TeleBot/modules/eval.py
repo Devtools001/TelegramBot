6200e0096b50e0d8d2608ebe2b097c17a8043d1e
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-28 16:12:56 +0530

    Update eval.py

--- TeleBot/modules/eval.py
+++ TeleBot/modules/eval.py
@@ -12,54 +12,38 @@ from pyrogram import filters, enums
 
 DEV_USERS = [5459540851]
 
-@app.on_message(filters.command(["shell", "sh"]))
-@app.on_edited_message(
-    filters.command(["shell", "sh"]))
-async def shell(_, m):
-    cmd = m.text.split(" ", 1)
-    if len(cmd) == 1:
-        return await m.reply("No command to execute was given.")
-    shell = (await shell_exec(cmd[1]))[0]
-    if len(shell) > 3000:
-        with open("shell_output.txt", "w") as file:
-            file.write(shell)
-        with open("shell_output.txt", "rb") as doc:
-            await m.reply_document(document=doc, file_name=doc.name)
-            try:
-                os.remove("shell_output.txt")
-            except:
-                pass
-    elif len(shell) != 0:
-        await m.reply(shell, parse_mode=enums.ParseMode.HTML)
-    else:
-        await m.reply("No Reply")
+async def aexec(code, client, message):
+    exec(
+        "async def __aexec(client, message): "
+        + "".join(f"\n {l_}" for l_ in code.split("\n"))
+    )
+    return await locals()["__aexec"](client, message)
 
-
-@app.on_message(filters.command(["ev", "run"]))
-@app.on_edited_message(filters.command(["ev", "run"]))
-async def evaluation_cmd_t(_, m):
-    status_message = await m.reply("__Processing eval pyrogram...__")
-    try:
-        cmd = m.text.split(" ", maxsplit=1)[1]
-    except IndexError:
-        return await status_message.edit("__No evaluate message!__")
-
+@app.on_message(filters.command(["run","eval", "e"],config.CMDS))
+async def eval(client, message):
+    if not message.from_user.id in config.DEVS:
+         return await message.reply_text("`You Don't Have Enough Rights To Run This!`")
+    if len(message.text.split()) <2:
+          return await message.reply_text("`Input Not Found!`")
+    status_message = await message.reply_text("Processing ...")
+    cmd = message.text.split(None, 1)[1]
+    start = datetime.now()
+    reply_to_ = message
+    if message.reply_to_message:
+        reply_to_ = message.reply_to_message
     old_stderr = sys.stderr
     old_stdout = sys.stdout
     redirected_output = sys.stdout = io.StringIO()
     redirected_error = sys.stderr = io.StringIO()
     stdout, stderr, exc = None, None, None
-
     try:
-        await aexec(cmd, _, m)
+        await aexec(cmd, client, message)
     except Exception:
         exc = traceback.format_exc()
-
     stdout = redirected_output.getvalue()
     stderr = redirected_error.getvalue()
     sys.stdout = old_stdout
     sys.stderr = old_stderr
-
     evaluation = ""
     if exc:
         evaluation = exc
@@ -69,38 +53,18 @@ async def evaluation_cmd_t(_, m):
         evaluation = stdout
     else:
         evaluation = "Success"
-
-    final_output = f"**EVAL**:\n`{cmd}`\n\n**OUTPUT**:\n`{evaluation.strip()}`\n"
-
+    end = datetime.now()
+    ping = (end-start).microseconds / 1000
+    final_output = "<b>📎 Input</b>: "
+    final_output += f"<code>{cmd}</code>\n\n"
+    final_output += "<b>📒 Output</b>:\n"
+    final_output += f"<code>{evaluation.strip()}</code> \n\n"
+    final_output += f"<b>✨ Taken Time</b>: {ping}<b>ms</b>"
     if len(final_output) > 4096:
-        with open("MissKatyEval.txt", "w+", encoding="utf8") as out_file:
-            out_file.write(final_output)
-        await status_message.reply_document(
-            document="MissKatyEval.txt",
-            caption=f"<code>{cmd[: 4096 // 4 - 1]}</code>",
-            disable_notification=True,
-        )
-        os.remove("MissKatyEval.txt")
-        await status_message.delete()
+        with io.BytesIO(str.encode(final_output)) as out_file:
+            out_file.name = "eval.text"
+            await reply_to_.reply_document(
+                document=out_file, caption=cmd, disable_notification=True
+            )
     else:
-        await status_message.edit(final_output, parse_mode=enums.ParseMode.MARKDOWN)
-
-
-async def aexec(code, client, message):
-    exec(
-        "async def __aexec(client, message): "
-        + "\n p = print"
-        + "".join(f"\n {l_}" for l_ in code.split("\n"))
-    )
-    return await locals()["__aexec"](client, message)
-
-
-async def shell_exec(code, treat=True):
-    process = await asyncio.create_subprocess_shell(
-        code, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.STDOUT
-    )
-
-    stdout = (await process.communicate())[0]
-    if treat:
-        stdout = stdout.decode().strip()
-    return stdout, process
+        await status_message.edit_text(final_output)
