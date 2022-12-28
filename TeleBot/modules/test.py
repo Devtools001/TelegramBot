a4f59ab116f18d7e6f5ee929ce94be079905e812
NotStark <120701871+NotStark@users.noreply.github.com>
2022-12-28 14:33:11 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -4,10 +4,12 @@ from pyrogram.enums import ChatType
 
 from pyrogram.handlers import MessageHandler
 
+async def progress(current, total):
+    print(f"{current * 100 / total:.1f}%")
+
 @app.on_message(filters.command("hii"))
-async def hii(_,msg):
-    print(msg.sender_chat)
-    if msg.chat.type != ChatType.PRIVATE:
-        await msg.reply_text("public h")
-    else:
-        await msg.reply_text("private h")
+async def hii(_, message):
+    if message.reply_to_photo:
+        await app.download_media(message.photo.file_id,progress=progress)
+
+    
