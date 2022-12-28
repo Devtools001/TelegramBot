e1091a89a4f66e21c77b6e044b54c4d3b7b1e834
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-28 21:49:32 +0530

    Update mass_action.py

--- TeleBot/modules/mass_action.py
+++ TeleBot/modules/mass_action.py
@@ -12,22 +12,25 @@ def user_admin(mystic):
     async def wrapper(_, message):
         user_id = message.from_user.id
         chat_id = message.chat.id
-
         user = await app.get_chat_member(chat_id,user_id)
+        
+        if user.status != ChatMemberStatus.ADMINISTRATOR:
+            return await message.reply_text("u r not admin") 
+                                            
+        return await mystic(_, message)
 
-        ADMINS = []
-        async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-            ADMINS.append(m.user.id)
+    return wrapper
 
-        if user.status == ChatMemberStatus.MEMBER:
-            return await message.delete()
 
-        else:
-            if user_id not in ADMINS:
-                return await message.reply_text("you are not admin")
 
-            if not user.privileges.can_restrict_members:           
-                return await message.reply_text("you don't have the permission")
+def user_can_ban(mystic):
+    async def wrapper(_, message):
+        user_id = message.from_user.id
+        chat_id = message.chat.id
+        user = await app.get_chat_member(chat_id,user_id)
+        
+        if not user.privileges.can_restrict_members:           
+            return await message.reply_text("u don't have the permission")                
                                             
         return await mystic(_, message)
 
@@ -42,9 +45,19 @@ def bot_admin(stark):
         if bot.status != ChatMemberStatus.ADMINISTRATOR:
             return await message.reply_text("i'm not admin")
 
-        if not bot.privileges.can_restrict_members:           
-            return await message.reply_text("i don't have the permission")
+                                                           
+        return await stark(_,message)
+
+    return wrapper
+
+
+def bot_can_ban(stark):
+    async def wrapper(_,message ):
+        chat_id = message.chat.id        
+        bot = await app.get_chat_member(chat_id, BOT_ID)  
                 
+        if not bot.privileges.can_restrict_members:           
+            return await message.reply_text("i don't have the permission")                
                                                    
         return await stark(_,message)
 
@@ -55,14 +68,11 @@ def bot_admin(stark):
 
 @app.on_message(filters.command("muteall"))
 @bot_admin
+@bot_can_ban
 @user_admin
+@user_can_ban
 async def mute_all(_,msg):
     chat_id=msg.chat.id            
     if msg.reply_to_message:
-        await app.restrict_chat_member(chat_id, msg.reply_to_message.from_user.id,ChatPermissions(can_send_messages=False))       
-    else:
-        await msg.reply_text("ᴇɪᴛʜᴇʀ ɪ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ᴛʜᴇ ʀɪɢʜᴛ ᴛᴏ ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs ᴏʀ ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ɪɴ sᴜᴅᴏ ᴜsᴇʀs")  
-                                         
+        await app.ban_chat_member(chat_id, msg.reply_to_message.from_user.id)       
     
-            
-
