6cd6ef39d30e7f0e481756c40b5b615a49f07049
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-29 21:22:52 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -2,12 +2,7 @@ from TeleBot import pgram as app
 from pyrogram import filters
 from pyrogram.enums import ChatType
 
-from pyrogram.handlers import MessageHandler
 
-async def progress(current, total):
-    print(f"{current * 100 / total:.1f}%")
-
-@app.on_message(filters.command("hii"))
-async def hii(_, message):
-    print(message.sender_chat.id)
-    print(message.from_user.id)
+@app.on_message(filters.command("stat") & filters.group)
+async def (_, message):
+    await message.reply_text(f"ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs ɪɴ {message.chat.title} :- {message.reply_to_message.id}")
