75c3d08c40effa7f6b72c38f71415343aa040c48
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-29 21:26:45 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -1,8 +1,8 @@
 from TeleBot import pgram as app
 from pyrogram import filters
-from pyrogram.enums import ChatType
+#from pyrogram.enums import ChatType
 
 
 @app.on_message(filters.command("stat") & filters.group)
 async def stat(_, message):
-    await message.reply_text(f"ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs ɪɴ {message.chat.title} :- {message.reply_to_message.id}")
+    await message.reply_text(f"ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs ɪɴ {message.chat.title} :- {message.id}")
