b1dc7362294e6925da3142442191ef1e514409d9
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-30 13:21:36 +0530

    Update and rename test.py to test.txt

--- TeleBot/modules/test.py
+++
@@ -1,7 +0,0 @@
-from TeleBot import pgram as app
-from pyrogram import filters
-
-
-@app.on_message(filters.command("stat") & filters.group)
-async def stat(_, message):
-    await message.reply_text(f"ᴛᴏᴛᴀʟ ᴍᴇssᴀɢᴇs ɪɴ {message.chat.title} :- {message.id}")
