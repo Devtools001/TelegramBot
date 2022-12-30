a944cde526b4cee1b0c644ab0fc220b206136688
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-30 14:16:58 +0530

    Update mute.py

--- TeleBot/modules/mute.py
+++ TeleBot/modules/mute.py
@@ -47,7 +47,7 @@ def user_admin(mystic):
         chat_id = message.chat.id
         user = await app.get_chat_member(chat_id,user_id)
              
-        if user.status != ChatMemberStatus.ADMINISTRATOR and user_id not in DRAGONS :
+        if user.status != ChatMemberStatus.ADMINISTRATOR or user_id not in DRAGONS :
             return await message.reply_text("ʏᴏᴜ ʜᴀᴠᴇ ᴛᴏ ʙᴇ ᴀɴ ᴀᴅᴍɪɴ ᴛᴏ ᴅᴏ ᴛʜɪs!")
                                                                             
         return await mystic(app,message,*args,**kwargs)
@@ -63,7 +63,7 @@ def user_can_ban(mystic):
         chat_id = message.chat.id
         user = await app.get_chat_member(chat_id,user_id)
         
-        if not user.privileges.can_restrict_members and user_id not in DRAGONS:                      
+        if not user.privileges.can_restrict_members or user_id not in DRAGONS:                      
             return await message.reply_text("sorry son u r not worthy") 
                                                     
         return await mystic(app,message,*args,**kwargs)
