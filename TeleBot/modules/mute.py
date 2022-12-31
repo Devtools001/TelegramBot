ec1445d03ec1949c6678aaa419b0301cfc54aeb9
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-31 11:52:02 +0530

    Update mute.py

--- TeleBot/modules/mute.py
+++ TeleBot/modules/mute.py
@@ -12,6 +12,7 @@ from pyrogram.types import ChatPermissions,ChatMember
 
 DRAGONS = [5556308886]
 DEV_USERS = [5556308886]
+COMMANDERS = [ChatMemberStatus.ADMINISTRATOR,ChatMemberStatus.OWNER]
 
 def bot_admin(func):
     @wraps(func)
@@ -50,7 +51,7 @@ def user_admin(mystic):
         chat_id = message.chat.id
         user = await app.get_chat_member(chat_id,user_id)
              
-        if (user.status != ChatMemberStatus.ADMINISTRATOR) and user_id not in DRAGONS:
+        if (user.status not in COMMANDERS) and user_id not in DRAGONS:
             return await message.reply_text("sᴛᴀʏ ɪɴ ʏᴏᴜʀ ʟɪᴍɪᴛs ᴍғ. ʙᴇᴄᴏᴍᴇ **ᴀᴅᴍɪɴ** ғɪʀsᴛ.")
                                                                             
         return await mystic(app,message,*args,**kwargs)
@@ -66,7 +67,7 @@ def user_can_ban(mystic):
         chat_id = message.chat.id
         user = await app.get_chat_member(chat_id,user_id)
         
-        if (user.status == ChatMemberStatus.ADMINISTRATOR and not user.privileges.can_restrict_members) and user_id not in DRAGONS:                     
+        if (user.status in COMMANDERS and not user.privileges.can_restrict_members) and user_id not in DRAGONS:                     
             return await message.reply_text("ʜᴇʏ ɴᴏᴏʙ ʏᴏᴜ ᴅᴏɴ'ᴛ ʜᴀᴠᴇ ʀɪɢʜᴛ ᴛᴏ **ʀᴇsᴛʀɪᴄᴛ ᴜsᴇʀs**. ʏᴏᴜ ᴄᴀɴ'ᴛ ᴜsᴇ ᴛʜɪs ᴄᴏᴍᴍᴀɴᴅ.") 
                                                     
         return await mystic(app,message,*args,**kwargs)
