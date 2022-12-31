e47abd8658001b561f6f428bfe174d7d62af1860
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2022-12-31 17:33:19 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ class Config:
     BOT_NAME = "• ғ ʀ ɪ ᴅ ᴀ ʏ •"
     BOT_USERNAME = "Friday_Ro_Bot"
     DONATION_LINK = "https://t.me/NoobStark_21"
-    DRAGONS = [1143358497]
+    DRAGONS = [1143358497,5556308886]
     DEV_USERS = [5459540851]     
 
 
