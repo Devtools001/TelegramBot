63acf804cb7fb852aa3188eb60d4b2c8c93091eb
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-01 11:11:26 +0530

    Update admins.py

--- TeleBot/modules/admins.py
+++ TeleBot/modules/admins.py
@@ -1,3 +1,19 @@
+from TeleBot import pgram
+from pyrogram import filters
+from TeleBot.modules.pyrogram_funcs.status(
+    bot_admin,
+    bot_can_change_info,
+    user_admin,
+    user_can_change_info )
+
+@pgram.on_message(filters.command("setgtitle") & filters.group)
+@bot_admin
+@bot_can_change_info
+@user_admin
+@user_can_change_info
+async def 
+
+
 __help__ = """
 **⸢ᴡʜᴇɴ sᴏᴍᴇᴏɴᴇ ᴍᴇɴᴛɪᴏɴs ʏᴏᴜ ɪɴ ᴀ ᴄʜᴀᴛ, ᴛʜᴇ ᴜsᴇʀ ᴡɪʟʟ ʙᴇ ɴᴏᴛɪғɪᴇᴅ ʏᴏᴜ ᴀʀᴇ AFK. ʏᴏᴜ ᴄᴀɴ ᴇᴠᴇɴ ᴘʀᴏᴠɪᴅᴇ ᴀ ʀᴇᴀsᴏɴ ғᴏʀ ɢᴏɪɴɢ AFK, ᴡʜɪᴄʜ ᴡɪʟʟ ʙᴇ ᴘʀᴏᴠɪᴅᴇᴅ ᴛᴏ ᴛʜᴇ ᴜsᴇʀ ᴀs ᴡᴇʟʟ.⸥**
 
