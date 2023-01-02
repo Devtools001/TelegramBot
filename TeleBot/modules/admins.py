83073fc702330f43ef018eb62fc27a4c25ff136f
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-02 08:54:24 +0530

    Update admins.py

--- TeleBot/modules/admins.py
+++ TeleBot/modules/admins.py
@@ -29,7 +29,7 @@ async def promote_demote(_, message):
     async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
         administrators.append(m.user.id)
     replied = message.reply_to_message
-    print(replied.user.id)
+    print(replied.from_user.id)
     if not user:
         return 
     
