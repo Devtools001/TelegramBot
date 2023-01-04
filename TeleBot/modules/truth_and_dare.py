a32236089cc3e1aeb19c5806f8106e485c22bc98
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-04 12:25:18 +0530

    Update truth_and_dare.py

--- TeleBot/modules/truth_and_dare.py
+++ TeleBot/modules/truth_and_dare.py
@@ -15,3 +15,15 @@ async def true(_, message):
         await message.reply_text(f"`{truth}`")
         return     
 
+@pgram.on_message(filters.command(["dare","hdare"]))
+async def _dare(_, message):
+    if message.command[0] == "dare":
+        url = "https://api.truthordarebot.xyz/v1/dare"
+        dare = requests.get(url).json()["question"]
+        await message.reply_text(f"`dare`")
+        return 
+    if message.command[0] == "hdare":
+        url = "https://api.truthordarebot.xyz/v1/dare"
+        dare = requests.get(url).json()["translations"]["hi"]
+        await message.reply_text(f"`dare`")
+        return 
