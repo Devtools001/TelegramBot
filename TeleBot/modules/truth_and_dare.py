abafa62db97585281cd3c171ef7d869fc68715e2
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-04 11:47:22 +0530

    Create truth_and_dare.py

---
+++ TeleBot/modules/truth_and_dare.py
@@ -0,0 +1,10 @@
+import requests
+from TeleBot import pgram
+from pyrogram import filters
+
+@pgram.on_message(filters.command("truth"))
+async def true(_, message):
+    print(message.command)
+    url = "https://api.truthordarebot.xyz/v1/dare"
+    truth = requests.get(url).json()["question"]
+    await message.reply_text(truth)
