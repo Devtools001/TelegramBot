081381e0ad37774b906483fae0dae03d3797b781
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-04 18:38:12 +0530

    Create test.py

---
+++ TeleBot/modules/test.py
@@ -0,0 +1,19 @@
+from TeleBot import pgram
+from pyrogram import filters, enums 
+
+
+@pgram.on_message(filters.command("adminlist") & ~filters.command)
+async def _adminlist(_, message):
+    chat_id = message.chat.id
+    chat_title = message.chat.title
+    text = await message.reply("ғᴇᴛᴄʜɪɴɢ ᴀᴅᴍɪɴs ʟɪsᴛ...")
+    administrators = []
+    async for m in app.get_chat_members(message.chat.id, filter=enums.ChatMembersFilter.ADMINISTRATORS):        
+        administrators.append(m)
+    
+
+    for admin in administrators:
+        user = admin.user
+        status = admin.status
+        custom_title = admin.custom_title
+        print(custom_title)
