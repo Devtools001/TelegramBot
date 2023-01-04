2a2acf9eb14e58ee3697fa16432c12d7b5ef8850
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-04 11:53:01 +0530

    Update truth_and_dare.py

--- TeleBot/modules/truth_and_dare.py
+++ TeleBot/modules/truth_and_dare.py
@@ -4,9 +4,9 @@ from pyrogram import filters
 
 @pgram.on_message(filters.command("truth"))
 async def true(_, message):
-    print(message.command)
-    a = message.text.split(None,1)[1]
-    print(a)
+    print(message.command[0])
+   # if message.command
+    
     url = "https://api.truthordarebot.xyz/v1/dare"
     truth = requests.get(url).json()["question"]
     await message.reply_text(f"`{truth}`")
