2d1357b6a6f15cadc4e8692f3dce4e5fe54d8ee9
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-04 12:06:42 +0530

    Update truth_and_dare.py

--- TeleBot/modules/truth_and_dare.py
+++ TeleBot/modules/truth_and_dare.py
@@ -2,7 +2,7 @@ import requests
 from TeleBot import pgram
 from pyrogram import filters
 
-@pgram.on_message(filters.command(["truth"))
+@pgram.on_message(filters.command(["truth","htruth"]))
 async def true(_, message):
     if message.command[0] == "truth":
         url = "https://api.truthordarebot.xyz/v1/dare"
