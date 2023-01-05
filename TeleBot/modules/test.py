7712d4889f17d6f9e368813b324847294f9c0158
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 12:30:24 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -32,7 +32,7 @@ async def _adminlist(_, message):
         else:
             name = user.mention
 
-        if  user.is_bot==True:
+        if  user.is_bot=True:
             administrators.remove(admin)
             continue
     
