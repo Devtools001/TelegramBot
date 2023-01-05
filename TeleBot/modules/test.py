005127c447ca9035172882c7b173c87530afa446
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 10:27:27 +0530

    Update test.py

--- TeleBot/modules/test.py
+++ TeleBot/modules/test.py
@@ -17,20 +17,21 @@ async def _adminlist(_, message):
     ADMINS = []
     async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
         ADMINS.append(m)        
-    text = "ᴀᴅᴍɪɴs ɪɴ {message.chat.title"
+    text = f"ᴀᴅᴍɪɴs ɪɴ {message.chat.title}"
 
     for owner in ADMINS  :
         if owner.user.first_name == "":
             owner_name = "☠ ᴅᴇʟᴇᴛᴇᴅ ᴀᴄᴄᴏᴜɴᴛ"
         else:
             name = owner.user.mention  
-        if owner.user.status == ChatMemberStatus.OWNER:
+        if owner.status == ChatMemberStatus.OWNER:
             text += "\n 🥀 ᴏᴡɴᴇʀ :"
             text += "\n • {name}\n"
 
-            if owner.user.custom_title:
+            if owner.custom_title:
                 text += f" ┗━ {custom_title}\n"
 
     await repl.edit(text)
+    print(text)
 
   
