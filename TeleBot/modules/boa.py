548cd57e1eb5db7992f3bb1b51ea788785a1c05b
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 13:12:11 +0530

    Update boa.py

--- TeleBot/modules/boa.py
+++ TeleBot/modules/boa.py
@@ -13,9 +13,31 @@ async def _adminlist(_, message):
             
         )
     
-    username=0
-    usertext = ''
-    async for m in pgram.get_chat_members(message.chat.id,filter=enums.ChatMembersFilter.BOTS):
-        username += 1
-        usertext += f"\n✨ [{m.user.first_name}](tg://user?id={m.user.id})"
-    await repl.edit(f"🤖bots in {message.chat.title}\n{usertext}")    
+    chat_title = message.chat.title 
+    chat_id = message.chat.id 
+
+    bots = []
+    async for m in app.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BOTS):
+        bots.append(m)
+
+    ADMINS_LIST = []
+    for bot in bots:
+        if user.user.username is not None:
+            ADMINS_LIST.append(f'- <a href=tg://user?id={bot.user.username}>{bot.user.first_name}</a> id `{bot.user.id}`\n')
+        else:
+            ADMINS_LIST.append(f'- <a href=tg://user?id={bot.user.id}>{bot.user.first_name}</a> id `{bot.user.id}`\n')
+
+
+    admin_header = f"Admins in {html.escape(chat_title)}:\n"
+    
+    for admin in ADMINS_LIST:
+        admin_header += admin
+    await message.reply(
+        (
+            f"{admin_header}\n\n"
+            "__These are the updated values.__"
+        ),
+        quote=True
+    )
+        
+    
