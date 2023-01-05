fc4c8038e729ad496a624f3bdececb4c3f88c4d6
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 17:36:05 +0530

    Update boa.py

--- TeleBot/modules/boa.py
+++ TeleBot/modules/boa.py
@@ -2,8 +2,6 @@ import time
 from TeleBot import pgram,get_readable_time
 from pyrogram import filters, enums 
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.enums import UserStatus
-from pyrogram.errors import FloodWait
 
 
 
@@ -25,39 +23,3 @@ async def _adminlist(_, message):
     await repl.edit(f"{header}\n\n")
                   
             
-@pgram.on_message(filters.command("kickthefools"))
-async def _kickthefools(_,message):      
-    text = await message.reply("ᴋɪᴄᴋɪɴɢ ᴍᴇᴍʙᴇʀs ᴡʜᴏ ᴡᴇʀᴇ ɪɴᴀᴄᴛɪᴠᴇ ғᴏʀ ᴀ ᴍᴏɴᴛʜ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ")  
-    chat_id = message.chat.id
-    x = 0
-    fools = []    
-    ADMINS = []  
-    start = time.time()     
-    async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
-        ADMINS.append(m.user.id)
-    
-    async for member in pgram.get_chat_members(chat_id) :          
-        user = member.user        
-        if user.status == UserStatus.LAST_MONTH:
-            if user.id in ADMINS :
-               pass
-            else:                
-                fools.append(member.user.id)  
-    if not fools:
-       await text.edit("ᴛʜᴇʀᴇ ᴀʀᴇɴ'ᴛ ᴀɴʏ ғᴏᴏʟs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ")
-    else:
-        for i in ADMINS:
-            try:                         
-                await pgram.ban_chat_member(chat_id,fools[x])           
-                await pgram.unban_chat_member(chat_id,fools[x])  
-                x += 1                
-            except IndexError:
-                pass
-            except FloodWait as e:
-                asyncio.sleep(e.value)
-        end = get_readable_time((time.time() - start))  
-        await text.delete()
-        await message.reply_text(f"ᴋɪᴄᴋᴇᴅ {len(fools)} ᴍᴇᴍʙᴇʀs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ ᴡʜᴏ ᴡᴇʀᴇ ɪɴᴀᴄᴛɪᴠᴇ ғᴏʀ ᴀ ᴍᴏɴᴛʜ.\n⏰ ᴛɪᴍᴇ ᴛᴏᴏᴋ : {end}")
-
-
-
