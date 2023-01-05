4a442e190073fc33d7517b543d8b508fe30af498
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 16:59:54 +0530

    Update boa.py

--- TeleBot/modules/boa.py
+++ TeleBot/modules/boa.py
@@ -3,6 +3,9 @@ from TeleBot import pgram,DEV_USERS, DRAGONS
 from pyrogram import filters, enums 
 from pyrogram.enums import ChatMemberStatus
 from pyrogram.enums import UserStatus
+from pyrogram.errors import FloodWait
+
+SUPREME_USERS = DRAGONS + DEV_USERS
 
 @pgram.on_message(filters.command("bots") & ~filters.private)
 async def _adminlist(_, message):       
@@ -21,25 +24,35 @@ async def _adminlist(_, message):
     await repl.edit(f"{header}\n\n")
                   
             
-@pgram.on_message(filters.command("kkick"))
-async def _kickthefools(_,message):    
+@pgram.on_message(filters.command("kickthefools"))
+async def _kickthefools(_,message):  
+    text = await message.reply(f"kicking fools")  
     chat_id = message.chat.id
-    z = []
-    
+    fools = []    
     ADMINS = []   
+    x = 0
     async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.ADMINISTRATORS):
         ADMINS.append(m.user.id)
     
     async for member in pgram.get_chat_members(chat_id) :          
-        user = member.user
-        
+        user = member.user        
         if user.status == UserStatus.RECENTLY:
             if user.id in ADMINS :
                pass
-            else:
-                
-                z.append(member.user.first_name)  
-    if not z:
-       print("empty")
+            else:                
+                fools.append(member.user.first_name)  
+    if not fools:
+       await message.reply_text("ᴛʜᴇʀᴇ ᴀʀᴇɴ'ᴛ ᴀɴʏ ғᴏᴏʟs ɪɴ ᴛʜɪs ɢʀᴏᴜᴘ")
     else:
-        print(z,len(z))
+        try:      
+                   
+            await pgram.ban_chat_member(chat_id,fools[x])
+            await message.reply_text(f"kicking fools")
+            await pgram.unban_chat_member(chat_id,member.user.id)  
+            x += 1
+        except FloodWait as e:
+            asyncio.sleep(e.value)
+        await text.edit(f"{len(fools)}")
+
+
+
