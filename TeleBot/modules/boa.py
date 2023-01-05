9ae6d382cf6984308015e6b61b6a5e933069224f
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 13:26:57 +0530

    Update boa.py

--- TeleBot/modules/boa.py
+++ TeleBot/modules/boa.py
@@ -8,28 +8,19 @@ from pyrogram.errors import BadRequest
 async def _adminlist(_, message):       
     chat_title = message.chat.title 
     chat_id = message.chat.id 
-
-    repl = await message.reply(
-            "» ғᴇᴛᴄʜɪɴɢ ʙᴏᴛs ʟɪsᴛ...",
-            
-        )
-        
+    repl = await message.reply("» ғᴇᴛᴄʜɪɴɢ ʙᴏᴛs ʟɪsᴛ...")                                        
     bots = []
     async for m in pgram.get_chat_members(chat_id, filter=enums.ChatMembersFilter.BOTS):
         bots.append(m)
-
     BOT_LIST = []
     for bot in bots:
         BOT_LIST.append(f"◎ {bot.user.mention}\n")
-
-    admin_header = f"🎣 ʙᴏᴛs ɪɴ {chat_title}:\n"
-    
+    admin_header = f"🎣 ʙᴏᴛs ɪɴ {chat_title}:\n\n"    
     for admin in BOT_LIST:
         admin_header += admin
-    await repl.edit(
-        
-            f"{admin_header}\n\n"
-            )
+    await repl.edit(f"{admin_header}\n\n")
+                  
+            
         
     
         
