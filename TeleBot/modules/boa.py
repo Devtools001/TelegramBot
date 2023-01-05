994ed69a0423e0ee40ee70e000caf4e9d7d55c7f
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-05 13:29:49 +0530

    Update boa.py

--- TeleBot/modules/boa.py
+++ TeleBot/modules/boa.py
@@ -1,8 +1,8 @@
-import html
+
 from TeleBot import pgram
 from pyrogram import filters, enums 
 from pyrogram.enums import ChatMemberStatus
-from pyrogram.errors import BadRequest 
+
 
 @pgram.on_message(filters.command("bots") & ~filters.private)
 async def _adminlist(_, message):       
@@ -15,9 +15,9 @@ async def _adminlist(_, message):
     BOT_LIST = []
     for bot in bots:
         BOT_LIST.append(f"◎ {bot.user.mention}\n")
-    admin_header = f"🎣 ʙᴏᴛs ɪɴ {chat_title}:\n\n"    
-    for admin in BOT_LIST:
-        admin_header += admin
+    header = f"🎣 ʙᴏᴛs ɪɴ {chat_title}:\n"    
+    for bumt in BOT_LIST:
+        header += bumt
     await repl.edit(f"{admin_header}\n\n")
                   
             
