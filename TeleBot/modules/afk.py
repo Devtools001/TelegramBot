4049b36081c0208e51132634ea85d9adc9c92582
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-06 13:12:15 +0530

    Update afk.py

--- TeleBot/modules/afk.py
+++ TeleBot/modules/afk.py
@@ -87,8 +87,7 @@ async def active_afk(_, message: Message):
                 f"**{message.from_user.first_name}** is back online",
                 disable_web_page_preview=True,
             )
-        await put_cleanmode(message.chat.id, send.message_id)
-        return
+        
     if len(message.command) == 1 and not message.reply_to_message:
         details = {
             "type": "text",
