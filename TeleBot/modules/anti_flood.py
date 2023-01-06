850fdbbf24c6ccc0166e522349f55048a842d45f
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-06 21:23:42 +0530

    Update anti_flood.py

--- TeleBot/modules/anti_flood.py
+++ TeleBot/modules/anti_flood.py
@@ -10,7 +10,19 @@ from TeleBot.modules.mongo.flood_mongo import (
     get_flood,
     get_floodlimit)
 
+from TeleBot.helpers.time_checker import time_string_helper
 
+@pgram.on_message(filters.command("flood"))
+@bot_admin,
+@bot_can_ban,
+@user_admin,
+@user_can_ban
+async def _flood(_, message):
+    chat_id = message.chat.id
+    if not await get_flood(chat_id):
+        return await message.reply_text("Tʜɪs ᴄʜᴀᴛ ɪsɴ'ᴛ ᴄᴜʀʀᴇɴᴛʟʏ ᴇɴғᴏʀᴄɪɴɢ ғʟᴏᴏᴅ ᴄᴏɴᴛʀᴏʟ")
+    
+        
 
 __help__ = """
 ⸢*ᴀɴᴛɪғʟᴏᴏᴅ* ᴀʟʟᴏᴡs ʏᴏᴜ ᴛᴏ ᴛᴀᴋᴇ ᴀᴄᴛɪᴏɴ ᴏɴ ᴜsᴇʀs ᴛʜᴀᴛ sᴇɴᴅ ᴍᴏʀᴇ ᴛʜᴀɴ x ᴍᴇssᴀɢᴇs ɪɴ ᴀ ʀᴏᴡ. ᴇxᴄᴇᴇᴅɪɴɢ ᴛʜᴇ sᴇᴛ ғʟᴏᴏᴅ ᴡɪʟʟ ʀᴇsᴜʟᴛ ɪɴ ʀᴇsᴛʀɪᴄᴛɪɴɢ ᴛʜᴀᴛ ᴜsᴇʀ.
