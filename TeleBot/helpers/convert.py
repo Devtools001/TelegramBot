bb6d555296ced2db950153854a9c036c3753d9f9
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-07 09:16:33 +0530

    Update convert.py

--- TeleBot/helpers/convert.py
+++ TeleBot/helpers/convert.py
@@ -1,21 +1,23 @@
 import math
-
-async def convert_time(given_time: int, time_format: str):
-    week = 518400
-    day = 86400 
-    hour = 3600 
-    minute = 60 
-
-    if time_format == 'w':
-        cal_time = given_time * week 
-    elif time_format == 'd':
-        cal_time = given_time * day 
-    elif time_format == 'h':
-        cal_time = given_time * hour
-    elif time_format == 'm':
-        cal_time = given_time * minute
-    
-    return cal_time
+from datetime import datetime,timedelta 
+from pyrogram.types import Message
+ 
+async def time_converter(message: Message, time_value: str) -> int:
+    unit = ["m", "h", "d"]  # m == minutes | h == hours | d == days
+    check_unit = "".join(list(filter(time_value[-1].lower().endswith, unit)))
+    currunt_time = datetime.now()
+    time_digit = time_value[:-1]
+    if not time_digit.isdigit():
+        return await message.reply_text("ɪɴᴄᴏʀʀᴇᴄᴛ ᴛɪᴍᴇ sᴘᴇᴄɪғɪᴇᴅ.")
+    if check_unit == "m":
+        temp_time = currunt_time + timedelta(minutes=int(time_digit))
+    elif check_unit == "h":
+        temp_time = currunt_time + timedelta(hours=int(time_digit))
+    elif check_unit == "d":
+        temp_time = currunt_time + timedelta(days=int(time_digit))
+    else:
+        return await message.reply_text("ɪɴᴄᴏʀʀᴇᴄᴛ ᴛɪᴍᴇ sᴘᴇᴄɪғɪᴇᴅ.")
+    return int(datetime.timestamp(temp_time))
 
 
 async def convert_size(size_bytes):
