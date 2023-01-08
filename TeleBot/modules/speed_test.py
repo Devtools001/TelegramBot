e2e673367535df60fd80d680fc9c4223c3974fd3
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 14:01:26 +0530

    Update speed_test.py

--- TeleBot/modules/speed_test.py
+++ TeleBot/modules/speed_test.py
@@ -22,22 +22,32 @@ async def _speed(_, message):
     
 
 @pgram.on_callback_query()
-async def _speedtest(app : Client,callback_query: CallbackQuery):   
-    text = await callback_query.message.edit("ʀᴜɴɴɪɴɢ ᴀ sᴩᴇᴇᴅᴛᴇsᴛ...")
-    speed = speedtest.Speedtest()
-    speed.get_best_server()
-    speed.download()
-    speed.upload()
-    msg = "sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ"
-    query = callback_query.data.lower()
-    if query == "speedtest_image":
-        speedtest_image = speed.results.share()
-        await callback_query.message.reply_photo(
+async def _speedtest(app : Client,callback_query: CallbackQuery): 
+    if callback_query.message.from_user.id in DEV_USERS :   
+        text = await callback_query.message.edit("ʀᴜɴɴɪɴɢ ᴀ sᴩᴇᴇᴅᴛᴇsᴛ...")
+        speed = speedtest.Speedtest()
+        speed.get_best_server()
+        speed.download()
+        speed.upload()
+        msg = "sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ"
+        query = callback_query.data.lower()
+        if query == "speedtest_image":
+            speedtest_image = speed.results.share()
+            await callback_query.message.reply_photo(
                 photo=speedtest_image, caption=msg
             )
-        await text.delete()
+            await text.delete()
 
-    if query == "speedtest_text":
-        result = speed.results.dict()
-        msg += f"\n**⦾ ᴘɪɴɢ »** `{result['ping']}`\n**⦾ ᴜᴘʟᴏᴀᴅ »** `{await convert(result['upload'])}Mb/s`\n**⦾ ᴅᴏᴡɴʟᴏᴀᴅ »** `{await convert(result['download'])}Mb/s"
-        await text.edit(msg, parse_mode=enums.ParseMode.MARKDOWN)
+        if query == "speedtest_text":
+            result = speed.results.dict()
+            msg += f"\n**⦾ ᴘɪɴɢ »** `{result['ping']}`\n**⦾ ᴜᴘʟᴏᴀᴅ »** `{await convert(result['upload'])}Mb/s`\n**⦾ ᴅᴏᴡɴʟᴏᴀᴅ »** `{await convert(result['download'])}Mb/s"
+            await text.edit(msg, parse_mode=enums.ParseMode.MARKDOWN)
+    else:
+        text = "ʙsᴅᴋ, ᴛᴇʀɪ ᴍᴀᴀ ᴋɪ ᴄʜᴜ* ʙʜᴀɢ ʏᴇʜᴀɴ sᴇ ʏᴇ ᴄᴏᴍᴍᴀɴᴅ sɪʀғ ᴅᴇᴠ ᴜsᴇ ᴋᴀʀ sᴀᴋᴛᴇ."
+        await app.answer_callback_query(
+        callback_query.id,
+        text=text,
+        show_alert=True)
+
+
+
