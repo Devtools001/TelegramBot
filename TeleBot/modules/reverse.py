5496923e729965504111a6fcbe4b41ff7b491c96
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 21:05:16 +0530

    Update reverse.py

--- TeleBot/modules/reverse.py
+++ TeleBot/modules/reverse.py
@@ -1,20 +1,23 @@
+import os
 from TeleBot import pgram
 from pyrogram import filters
 from GoogleSearch import Search
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 from GoogleSearch import Search
+from pyrogram.errors import BadRequest
 
 @pgram.on_message(filters.command(["pp","p","grs","reverse"]))
 async def _reverse(_, message):
-    if len(message.command) >= 2:
+    if len(message.command) == 2:
         url = message.text.split(None,1)[1]
         if url.startswith(("https://", "http://")):
             msg = await message.reply_text("ᴜᴘʟᴏᴀᴅɪɴɢ ᴜʀʟ ᴛᴏ ɢᴏᴏɢʟᴇ..")
             result = Search(url=url)
             name = result["output"]
             link = result["similar"]
-            await msg.edit_text("ᴜᴘʟᴏᴀᴅᴇᴅ ᴛᴏ ɢᴏᴏɢʟᴇ, ғᴇᴛᴄʜɪɴɢ ʀᴇsᴜʟᴛs...")
-            await msg.edit_text(
+            try:
+                await msg.edit_text("ᴜᴘʟᴏᴀᴅᴇᴅ ᴛᴏ ɢᴏᴏɢʟᴇ, ғᴇᴛᴄʜɪɴɢ ʀᴇsᴜʟᴛs...")
+                await msg.edit_text(
                     text=f"{name}",
                     reply_markup=InlineKeyboardMarkup(
                         [
@@ -27,8 +30,30 @@ async def _reverse(_, message):
                         ],
                     ),
                 )
+            except BadRequest as er :
+                await message.reply_text(er)     
             return
         else:
             await message.reply_text(
                 "ᴄᴏᴍᴍᴀɴᴅ ᴍᴜsᴛ ʙᴇ ᴜsᴇᴅ ᴡɪᴛʜ ᴀ ʀᴇᴘʟʏ ᴛᴏ ᴀɴ ɪᴍᴀɢᴇ ᴏʀ sʜᴏᴜʟᴅ ɢɪᴠᴇ ᴜʀʟ"
-            )           
+            )   
+    replied = message.reply_to_message
+
+    elif replied and replied.photo:
+        try:
+            edit = await message.reply_text("ᴅᴏᴡɴʟᴏᴀᴅɪɴɢ ɪᴍᴀɢᴇ")
+        except BadRequest:
+            return
+
+    photo = await pgram.download_media(replied.photo.file_id,file_name = "reverse.jpg")
+    print(photo)
+    os.remove(photo)
+
+
+
+
+
+
+
+
+        
