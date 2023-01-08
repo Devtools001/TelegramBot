21e89c2c433b2ac4d1a3d41710bf37061ab7e95b
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 07:21:25 +0530

    Update anime.py

--- TeleBot/modules/anime.py
+++ TeleBot/modules/anime.py
@@ -1,2 +1,28 @@
+import random 
+import time
+import os
+import asyncio
+import requests 
 from TeleBot import pgram
 from pyrogram import filters
+from types import NoneType
+from pyrogram import filters, Client
+from pyrogram.errors import UserNotParticipant, WebpageCurlFailed, WebpageMediaEmpty
+from pyrogram.enums import ChatMemberStatus, ChatType
+from pyrogram.types import (
+    CallbackQuery,
+    InlineKeyboardButton,
+    InlineKeyboardMarkup,
+    InputMediaPhoto,
+    Message
+)
+
+from TeleBot import (
+    ANILIST_CLIENT,
+    ANILIST_REDIRECT_URL,
+    ANILIST_SECRET,
+    OWNER,
+    TRIGGERS as trg,
+    BOT_NAME,
+    anibot
+)
