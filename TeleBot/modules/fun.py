ba25be45c96203eabf5ff47fcea9cda135c64a90
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 12:17:43 +0530

    Create fun.py

---
+++ TeleBot/modules/fun.py
@@ -0,0 +1,11 @@
+import random
+from TeleBot import pgram
+from TeleBot.modules.fun_strings import *
+
+
+from pyrogram.errors import BadRequest
+
+
+@pgram.on_message(filters.command("runs"))
+async def _run(_, message):
+    await message.reply_text(random.choice(RUN_STRINGS))             
