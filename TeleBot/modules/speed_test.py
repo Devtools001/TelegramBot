897756bd439b3a92c220e8f1dbca9948a18af47b
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 13:29:51 +0530

    Update speed_test.py

--- TeleBot/modules/speed_test.py
+++ TeleBot/modules/speed_test.py
@@ -5,7 +5,7 @@ from pyrogram import filters, Client
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , CallbackQuery 
 
 
- buttons = [
+buttons = [
         [
             InlineKeyboardButton("ɪᴍᴀɢᴇ", callback_data="speedtest_image"),
             InlineKeyboardButton("ᴛᴇxᴛ", callback_data="speedtest_text"),
