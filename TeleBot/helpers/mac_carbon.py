bf5674b881c629155179c2e7d9a1d863c2c8f782
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 11:40:42 +0530

    Update mac_carbon.py

--- TeleBot/helpers/mac_carbon.py
+++ TeleBot/helpers/mac_carbon.py
@@ -1,3 +1,4 @@
+import aiofiles
 from asyncio import gather
 from datetime import datetime, timedelta
 from io import BytesIO
@@ -7,8 +8,14 @@ from random import randint
 from re import findall
 from re import sub as re_sub
 from sys import executable
-from TeleBot import ClientSession
+from TeleBot import aiohttpsession
 
-import aiofiles
 from PIL import Image
 from pyrogram.types import Message
+
+async def make_carbon(code):
+    url = "https://carbonara.vercel.app/api/cook"
+    async with aiohttpsession.post(url, json={"code": code}) as resp:
+        image = BytesIO(await resp.read())
+    image.name = "Friday.png"
+    return image
