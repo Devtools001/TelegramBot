61475ce3cb606b042216a60f2c962e27d8b44800
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 12:32:59 +0530

    Update fun.py

--- TeleBot/modules/fun.py
+++ TeleBot/modules/fun.py
@@ -6,6 +6,50 @@ from pyrogram import filters
 from pyrogram.errors import BadRequest
 
 
+@pgram.on_message(filters.command("toss"))
+async def _run(_, message):
+    await message.reply_text(random.choice(TOSS))             
+
+
 @pgram.on_message(filters.command("runs"))
 async def _run(_, message):
     await message.reply_text(random.choice(RUN_STRINGS))             
+
+
+@pgram.on_message(filters.command("dice"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "🎲")
+
+
+@pgram.on_message(filters.command("dart"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "🎯")
+
+@pgram.on_message(filters.command("slot"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "🎰")
+
+
+@pgram.on_message(filters.command("football"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "⚽")
+
+
+@pgram.on_message(filters.command("basket"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "🏀")
+
+
+@pgram.on_message(filters.command("bowling"))
+async def _run(_, message):
+    chat_id = message.chat.id
+    await pgram.send_dice(chat_id, "🎳")
+
+
+
+
