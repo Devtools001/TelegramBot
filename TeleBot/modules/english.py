8818768560178b0f564c827e49f538063c521513
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 13:08:07 +0530

    Update english.py

--- TeleBot/modules/english.py
+++ TeleBot/modules/english.py
@@ -34,4 +34,46 @@ async def _atony(_, message):
     await message.reply_text(f"`{got}`") 
 
 
+@pgram.on_message(filters.command("synonyms"))
+async def _atony(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("ᴜsᴀɢᴇ :  /sʏɴᴏɴʏᴍs <ᴡᴏʀᴅ>: ғɪɴᴅ ᴛʜᴇ sʏɴᴏɴʏᴍs ᴏғ ᴀ ᴡᴏʀᴅ")
+    text = message.text.split(None,1)[1]  
+    let = dictionary.synonym(text)
+    set = str(let)
+    jet = set.replace("{", "")
+    net = jet.replace("}", "")
+    got = net.replace("'", "")
+    await event.reply_text(f"`{got}`")
+
+
+@pgram.on_message(filters.command("spell"))
+async def _spell(_, message):
+    replied = message.reply_to_message
+    if not replied.text:
+        return await message.reply_text("ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ʙᴀʙʏ.")
+    ctext = replied
+    msg = ctext.text
+    print(msg)
+    params = dict(lang="US", clientVersion="2.0", apiKey=API_KEY, text=msg)
+
+    res = requests.get(URL, params=params)
+    changes = json.loads(res.text).get("LightGingerTheTextResult")
+    curr_string = ""
+    prev_end = 0
+
+    for change in changes:
+        start = change.get("From")
+        end = change.get("To") + 1
+        suggestions = change.get("Suggestions")
+        if suggestions:
+            sugg_str = suggestions[0].get("Text")
+            curr_string += msg[prev_end:start] + sugg_str
+            prev_end = end
+
+    curr_string += msg[prev_end:]
+    await message.reply_text(curr_string)
+
+
+
      
