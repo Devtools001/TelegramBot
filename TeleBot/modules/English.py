4017a5a944a00c2dd62e7d15cf3ebc0e10d8fda0
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-08 12:47:56 +0530

    Create English.py

---
+++ TeleBot/modules/English.py
@@ -0,0 +1,20 @@
+from TeleBot import pgram
+import json
+import requests
+from PyDictionary import PyDictionary
+
+
+API_KEY = "6ae0c3a0-afdc-4532-a810-82ded0054236"
+URL = "http://services.gingersoftware.com/Ginger/correct/json/GingerTheText"
+
+@pgram.on_message(filters.command("define"))
+async def _define(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("ɢɪᴠᴇ ᴀ ᴛᴇxᴛ ᴛᴏ ᴅᴇғɪɴᴇ ɪᴛ.")
+    text = message.text.split(None,1)[1]
+    let = dictionary.meaning(text)
+    set = str(let)
+    jet = set.replace("{", "")
+    net = jet.replace("}", "")
+    got = net.replace("'", "")
+    await message.reply_text(got)
