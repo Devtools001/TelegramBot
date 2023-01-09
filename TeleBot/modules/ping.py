a699a638555e9a7c7419367c7f5925f8ef7a6286
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-09 08:57:32 +0530

    Update ping.py

--- TeleBot/modules/ping.py
+++ TeleBot/modules/ping.py
@@ -12,9 +12,9 @@ async def _ping(_, message):
     telegram_ping = str(round((end - start) * 1000, 3)) + " ms"
     uptime = get_readable_time((time.time() - StartTime))
     await msg.edit(f"""
-        𝗣𝗢𝗡𝗚 🥀!!\n"
-        "**ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** {telegram_ping}\n"
-        "**sᴇʀᴠɪᴄᴇ ᴜᴘᴛɪᴍᴇ:** {uptime}"""       
+        𝗣𝗢𝗡𝗚 🥀!!
+        **ᴛɪᴍᴇ ᴛᴀᴋᴇɴ:** {telegram_ping}
+        **sᴇʀᴠɪᴄᴇ ᴜᴘᴛɪᴍᴇ:** {uptime}"""       
     )
 
 
