1fb7531efed56e38ff68d9388c290002d4216ac1
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:06:16 +0530

    Update dev.py

--- TeleBot/modules/dev.py
+++ TeleBot/modules/dev.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from contextlib import suppress
 
 
-@pgram.on_message(filters.command("leave") & filter.user(DEV_USERS))
+@pgram.on_message(filters.command("leave") & filters.user(DEV_USERS))
 async def _leave(_, message):
     if len(message.command) < 2:
         return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
