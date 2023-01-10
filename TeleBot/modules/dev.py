65c1c45a771d5693a5310365b1b00450a3eda410
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:23:48 +0530

    Update dev.py

--- TeleBot/modules/dev.py
+++ TeleBot/modules/dev.py
@@ -8,7 +8,7 @@ async def _leave(_, message):
     if len(message.command) < 2:
         return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
     chat_id = message.text.split(None,1)[1].strip()
-    if chat_id.isdigit():       
+    if chat_id.isdigit() or chat_id.startswith("-100"):       
         chat_id = int(chat_id)        
     else:
         return await message.reply_text("ʙᴀᴋᴀᴀᴀ ɪᴛ's ɴᴏᴛ ᴀ ᴄʜᴀᴛ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ɪs ᴀɴ ɪɴᴛᴇɢᴇʀ ɴᴏᴛ ᴀ sᴛʀɪɴɢ.")
