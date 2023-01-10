760d64d771bf628f58ecdbd7448f44eb682e2b71
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:41:17 +0530

    Update dev.py

--- TeleBot/modules/dev.py
+++ TeleBot/modules/dev.py
@@ -3,7 +3,7 @@ from pyrogram import filters
 from contextlib import suppress
 from pyrogram.errors import BadRequest ,Unauthorized
 
-@pgram.on_message(filters.command("leave") & filters.user(DEV_USERS))
+@pgram.on_message(filters.command(["leave","dleave"]) & filters.user(DEV_USERS))
 async def _leave(_, message):
     if len(message.command) < 2:
         return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
@@ -13,10 +13,19 @@ async def _leave(_, message):
         chat_id = int(chat_id)        
     else:
         return await message.reply_text("ʙᴀᴋᴀᴀᴀ ɪᴛ's ɴᴏᴛ ᴀ ᴄʜᴀᴛ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ɪs ᴀɴ ɪɴᴛᴇɢᴇʀ ɴᴏᴛ ᴀ sᴛʀɪɴɢ.")
-    try:    
-        await pgram.leave_chat(chat_id)
-    except BadRequest:
-        return await message.reply_text("ʙᴇᴇᴘ ʙᴏᴏᴘ, I ᴄᴏᴜʟᴅ ɴᴏᴛ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ(ᴅᴜɴɴᴏ ᴡʜʏ ᴛʜᴏ). 🙃")
-    with suppress(Unauthorized):
-        return await pgram.send_message(user_id,"ʙᴇᴇᴘ ʙᴏᴏᴘ, I ʟᴇғᴛ ᴛʜᴀᴛ sᴏᴜᴘ!.")
+    if message.command[0] == "leave":
+        try:    
+            await pgram.leave_chat(chat_id)
+        except BadRequest:
+            return await message.reply_text("ʙᴇᴇᴘ ʙᴏᴏᴘ, I ᴄᴏᴜʟᴅ ɴᴏᴛ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ(ᴅᴜɴɴᴏ ᴡʜʏ ᴛʜᴏ). 🙃")
+        with suppress(Unauthorized):
+            return await pgram.send_message(user_id,"ʙᴇᴇᴘ ʙᴏᴏᴘ, I ʟᴇғᴛ ᴛʜᴀᴛ sᴏᴜᴘ!.")
+    if message.command[0] == "dleave":
+        try:    
+            await pgram.leave_chat(chat_id,delete=True)
+        except BadRequest:
+            return await message.reply_text("ʙᴇᴇᴘ ʙᴏᴏᴘ, I ᴄᴏᴜʟᴅ ɴᴏᴛ ʟᴇᴀᴠᴇ ᴛʜᴀᴛ ɢʀᴏᴜᴘ(ᴅᴜɴɴᴏ ᴡʜʏ ᴛʜᴏ). 🙃")
+        with suppress(Unauthorized):
+            return await pgram.send_message(user_id,"ʙᴇᴇᴘ ʙᴏᴏᴘ, I ʟᴇғᴛ ᴛʜᴀᴛ sᴏᴜᴘ!. ᴀʟsᴏ ᴅᴇʟᴇᴛᴇᴅ ᴛʜᴇ ᴅɪᴀʟᴏɢs.")
+
 
