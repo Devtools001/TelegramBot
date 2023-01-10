11bf4d740de21d9f62dc789311c5a27ffbd6017e
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:05:42 +0530

    Create dev.py

---
+++ TeleBot/modules/dev.py
@@ -0,0 +1,11 @@
+from TeleBot import pgram,DEV_USERS
+from pyrogram import filters
+from contextlib import suppress
+
+
+@pgram.on_message(filters.command("leave") & filter.user(DEV_USERS))
+async def _leave(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
+    chat_id = message.text.split(None,1)[1].strip()
+    print(chat_id) 
