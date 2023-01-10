93748b5234478a30d9776f7379f3626f6c9e054a
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:11:19 +0530

    Update dev.py

--- TeleBot/modules/dev.py
+++ TeleBot/modules/dev.py
@@ -7,8 +7,8 @@ from contextlib import suppress
 async def _leave(_, message):
     if len(message.command) < 2:
         return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
-    chat_id = message.text.split(None,1)[1].strip()
-    if not chat_id.is_digit():
+    chat_id = str(message.text.split(None,1)[1].strip())
+    if not chat_id.is_int():
         return await message.reply_text("ʙᴀᴋᴀᴀᴀ ɪᴛ's ɴᴏᴛ ᴀ ᴄʜᴀᴛ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ɪs ᴀɴ ɪɴᴛᴇɢᴇʀ ɴᴏᴛ ᴀ sᴛʀɪɴɢ.")
     else:
         chat_id = int(chat_id)
