dfe22cfdaa10c88716638305297a35f1f14bdede
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 16:49:45 +0530

    Create pin.py

---
+++ TeleBot/modules/pin.py
@@ -0,0 +1,21 @@
+from TeleBot import pgram
+from pyrogram import filters 
+from TeleBot.modules.pyrogram_funcs.status import (
+    bot_admin,
+    user_admin,
+    bot_can_pin)
+
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup 
+@pgram.on_message(filters.command("pin") & ~filters.private)
+@bot_admin
+@user_admin
+@bot_can_pin
+async def _pin(_, message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply_text("💌 ʜᴇʏ ʙᴀʙʏ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴘɪɴ ɪᴛ.")
+    try:
+        await replied.pin()
+        await message.reply_text(💌 sᴜᴄᴄᴇss! ᴘɪɴɴᴇᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴏɴ ᴛʜɪs ɢʀᴏᴜᴘ.",reply_markup=
+        InlineKeyboardMarkup([[InlineKeyboardButton(📝 ᴠɪᴇᴡ ᴍᴇssᴀɢᴇ,url=replied.link)]]))
+    
