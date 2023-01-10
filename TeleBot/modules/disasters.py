09526e007c5965c40c01a7bbf70de0271b09b6a9
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 12:36:00 +0530

    Create disasters.py

---
+++ TeleBot/modules/disasters.py
@@ -0,0 +1,53 @@
+import os
+from TeleBot import pgram, DRAGONS,BOT_ID,DEV_USERS
+from pyrogram import filters ,enums
+from typing import Optional
+import json
+from TeleBot.modules.pyrogram_funcs.extracting_id import extract_user_id
+
+ELEVATED_USERS_FILE = os.path.join(os.getcwd(), "TeleBot/elevated_users.json")
+
+
+async def chech_user_id(user_id : int) -> Optional[str]:
+    if not user_id:
+        reply = "That...is a chat! baka ka omae?"
+
+    elif user_id == BOT_ID:
+        reply = "This does not work that way."
+
+    else:
+        reply = None
+    return 
+
+@pgram.on_message(filters.command("addsudo"))
+async def _addsudo(_, message):
+    user_id = extract_user_id(message)
+    member = await app.get_chat_member(message.chat.id, user_id)
+    reply = await check_user_id(user_id)
+    rt = ""
+
+    with open(ELEVATED_USERS_FILE, "r") as infile:
+        data = json.load(infile)
+
+    if reply:
+        await message.reply_text(reply)
+        return ""
+    if  user_id in DRAGONS:
+        await message.reply_text("This member is already a Dragon Disaster")
+    if  user_id in DEV_USERS:
+        await message.reply_text("This member is already a dev Disaster")
+
+    data["sudos"].append(user_id)
+    DRAGONS.append(user_id)
+    
+    with open(ELEVATED_USERS_FILE, "w") as outfile:
+        json.dump(data, outfile, indent=4)
+
+    await await message.reply_text(
+        ʀᴛ
+        + "\nsᴜᴄᴄᴇssғᴜʟʟʏ sᴇᴛ ᴅɪsᴀsᴛᴇʀ ʟᴇᴠᴇʟ ᴏғ {} ᴛᴏ ᴅʀᴀɢᴏɴ!".format(
+            member.mention,
+        ),
+    )
+    
+        
