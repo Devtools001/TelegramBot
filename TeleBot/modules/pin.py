051efb2674d6bec5446542a1d9f502e7b5b0d44b
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 17:25:52 +0530

    Update pin.py

--- TeleBot/modules/pin.py
+++ TeleBot/modules/pin.py
@@ -22,19 +22,25 @@ async def _pin(_, message):
         await message.reply_text(er)
 
 
-@pgram.on_message(filters.command("unpin") & ~filters.private)
+@pgram.on_message(filters.command(["unpin","unpinall"]) & ~filters.private)
 @bot_admin
 @user_admin
 @bot_can_pin
 async def _pin(_, message):
-    replied = message.reply_to_message
-    if not replied:
-        return await message.reply_text("🚫 ʜᴇʏ ʙᴀʙʏ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴜɴᴘɪɴ ɪᴛ.")
-    try:
-        await replied.unpin()
-        await message.reply_text("🚫 sᴜᴄᴄᴇss!  ᴜɴᴘɪɴɴᴇᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴏɴ ᴛʜɪs ɢʀᴏᴜᴘ.",reply_markup=
-        InlineKeyboardMarkup([[InlineKeyboardButton(text="🎣 ᴠɪᴇᴡ ᴍᴇssᴀɢᴇ",url=replied.link)],[InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ", callback_data="close")]]))  
-    except Exception as er:
-        await message.reply_text(er)
+    if message.command[0] == "unpin":
+        replied = message.reply_to_message
+        if not replied:
+            return await message.reply_text("🚫 ʜᴇʏ ʙᴀʙʏ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴜɴᴘɪɴ ɪᴛ.")
+        try:
+            await replied.unpin()
+            await message.reply_text("🚫 sᴜᴄᴄᴇss!  ᴜɴᴘɪɴɴᴇᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴏɴ ᴛʜɪs ɢʀᴏᴜᴘ.",reply_markup=
+            InlineKeyboardMarkup([[InlineKeyboardButton(text="🎣 ᴠɪᴇᴡ ᴍᴇssᴀɢᴇ",url=replied.link)],[InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ", callback_data="close")]]))  
+        except Exception as er:
+            await message.reply_text(er)
+    if message.command[0] == "unpinall":
+        await pgram.unpin_all_chat_messages(message.chat.id)
+        await message.reply_text("🎣 ᴜɴᴘɪɴɴᴇᴅ ᴀʟʟ ᴍᴇssᴀɢᴇs ɪɴ ᴛʜɪs ᴄʜᴀᴛ.", reply_markup=
+        InlineKeyboardMarkup([[InlineKeyboardButton("❌ ᴄʟᴏsᴇ", callback_data="close")]])
+
 
         
