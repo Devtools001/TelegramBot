adcffbf2c0b339efb36e6c4a464fc4e628b9ea6e
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 13:17:59 +0530

    Update dev.py

--- TeleBot/modules/dev.py
+++ TeleBot/modules/dev.py
@@ -8,7 +8,7 @@ async def _leave(_, message):
     if len(message.command) < 2:
         return await message.reply_text("ɢɪᴠᴇ ᴍᴇ ᴀ ᴄʜᴀᴛ ɪᴅ ʙᴀᴋᴀ.")
     chat_id = message.text.split(None,1)[1].strip()
-    if not chat_id.is_int():       
+    if not is_int(chat_id):       
         return await message.reply_text("ʙᴀᴋᴀᴀᴀ ɪᴛ's ɴᴏᴛ ᴀ ᴄʜᴀᴛ ɪᴅ, ᴄʜᴀᴛ ɪᴅ ɪs ᴀɴ ɪɴᴛᴇɢᴇʀ ɴᴏᴛ ᴀ sᴛʀɪɴɢ.")
     else:
         chat_id = int(chat_id)
