b49ec7fa9f116c1ba2651f41a2c4f7c2eb890d2f
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-10 17:14:32 +0530

    Update pin.py

--- TeleBot/modules/pin.py
+++ TeleBot/modules/pin.py
@@ -19,6 +19,22 @@ async def _pin(_, message):
         await message.reply_text("📝 sᴜᴄᴄᴇss! ᴘɪɴɴᴇᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴏɴ ᴛʜɪs ɢʀᴏᴜᴘ.",reply_markup=
         InlineKeyboardMarkup([[InlineKeyboardButton(text="💌 ᴠɪᴇᴡ ᴍᴇssᴀɢᴇ",url=replied.link)],[InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ", callback_data="close")]]))  
     except Exception as er:
-        print(er) 
+        await message.reply_text(er)
+
+
+@pgram.on_message(filters.command("unpin") & ~filters.private)
+@bot_admin
+@user_admin
+@bot_can_pin
+async def _pin(_, message):
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply_text("🚫 ʜᴇʏ ʙᴀʙʏ ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ ᴜɴᴘɪɴ ɪᴛ.")
+    try:
+        await replied.unpin()
+        await message.reply_text("🚫 sᴜᴄᴄᴇss!  ᴜɴᴘɪɴɴᴇᴅ ᴛʜɪs ᴍᴇssᴀɢᴇ ᴏɴ ᴛʜɪs ɢʀᴏᴜᴘ.",reply_markup=
+        InlineKeyboardMarkup([[InlineKeyboardButton(text="🎣 ᴠɪᴇᴡ ᴍᴇssᴀɢᴇ",url=replied.link)],[InlineKeyboardButton(text="❌ ᴄʟᴏsᴇ", callback_data="close")]]))  
+    except Exception as er:
+        await message.reply_text(er)
 
         
