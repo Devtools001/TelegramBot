5dfc005877702c88d57412cd16aabefbe296ad74
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-11 21:28:35 +0530

    Update speed_test.py

--- TeleBot/modules/speed_test.py
+++ TeleBot/modules/speed_test.py
@@ -5,6 +5,7 @@ from pyrogram import filters, Client ,enums
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup , CallbackQuery 
 
 S = DEV_USERS + DRAGONS 
+
 buttons = [
         [
             InlineKeyboardButton("ɪᴍᴀɢᴇ", callback_data="speedtest_image"),
@@ -21,28 +22,29 @@ async def _speed(_, message):
     await message.reply_text("sᴩᴇᴇᴅᴛᴇsᴛ ᴍᴏᴅᴇ", reply_markup=InlineKeyboardMarkup(buttons))
     
 
-@pgram.on_callback_query()
-async def _speedtest(app : Client,callback_query: CallbackQuery): 
-    if callback_query.from_user.id in (DEV_USERS or DRAGONS) :   
-        text = await callback_query.message.edit("ʀᴜɴɴɪɴɢ ᴀ sᴩᴇᴇᴅᴛᴇsᴛ...")
-        speed = speedtest.Speedtest()
-        speed.get_best_server()
-        speed.download()
-        speed.upload()
-        msg = "sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ"
-        query = callback_query.data.lower()
-        if query == "speedtest_image":
-            speedtest_image = speed.results.share()
-            await callback_query.message.reply_photo(
+@pgram.on_callback_query(filters.regex("speedtest_image"))
+async def _speedtest_img(app : Client,callback_query: CallbackQuery): 
+    text = await callback_query.message.edit("ʀᴜɴɴɪɴɢ ᴀ sᴩᴇᴇᴅᴛᴇsᴛ...")
+    speed = speedtest.Speedtest()
+    speed.get_best_server()
+    speed.download()
+    speed.upload()
+    msg = "sᴩᴇᴇᴅᴛᴇsᴛ ʀᴇsᴜʟᴛ"    
+    speedtest_image = speed.results.share()
+    await callback_query.message.reply_photo(
                     photo=speedtest_image, caption=msg
                 )
-            await text.delete()
+    await text.delete()
 
-        if query == "speedtest_text":
-            result = speed.results.dict()
-            msg += f"\n**⦾ ᴘɪɴɢ »** `{result['ping']}`\n**⦾ ᴜᴘʟᴏᴀᴅ »** `{await convert(result['upload'])}Mb/s`\n**⦾ ᴅᴏᴡɴʟᴏᴀᴅ »** `{await convert(result['download'])}Mb/s"
-            await text.edit(msg, parse_mode=enums.ParseMode.MARKDOWN)
+@pgram.on_callback_query(filters.regex("speedtest_text"))
+async def _speedtest_img(app : Client,callback_query: CallbackQuery):
+    text = await callback_query.message.edit("ʀᴜɴɴɪɴɢ ᴀ sᴩᴇᴇᴅᴛᴇsᴛ...")
+    result = speed.results.dict()
+    msg += f"\n**⦾ ᴘɪɴɢ »** `{result['ping']}`\n**⦾ ᴜᴘʟᴏᴀᴅ »** `{await convert(result['upload'])}Mb/s`\n**⦾ ᴅᴏᴡɴʟᴏᴀᴅ »** `{await convert(result['download'])}Mb/s"
+    await text.edit(msg, parse_mode=enums.ParseMode.MARKDOWN)
  
+
+            
 __help__ = """
 **⸢ᴄʜᴇᴄᴋ ᴍʏ sᴘᴇᴇᴅ⸥**
 
