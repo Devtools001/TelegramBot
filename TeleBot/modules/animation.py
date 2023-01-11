19680cc43e7e1df25d7b17d1c7f9e286b679bddf
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-11 21:05:25 +0530

    Update animation.py

--- TeleBot/modules/animation.py
+++ TeleBot/modules/animation.py
@@ -24,8 +24,8 @@ earth = [
 
 @pgram.on_message(filters.command("earth"))
 async def _earth(_, message):
-    x = await message.reply("🌍")
+    msg = await message.reply("🌍")
     for x in range(18):
-        await x.edit(earth[x % 18])
+        await msg.edit(earth[x % 18])
         await asyncio.sleep(1)
-    await x.edit_("🌏")
+    await msg.edit_("🌏")
