d32fb648b173833cd5fa169695df7bc93da1f224
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-11 20:26:59 +0530

    Update schedule.py

--- TeleBot/modules/schedule.py
+++ TeleBot/modules/schedule.py
@@ -1,44 +1,21 @@
 import requests 
 from TeleBot import pgram
 from pyrogram import filters
-from pyrogram import Client
-from pyrogram.types import Message ,CallbackQuery
-from datetime import datetime 
 
-async def get_scheduled(x: int = 9):
-    base_url = "https://api.jikan.moe/v4/schedules/"
-    day = str(day_(x if x!=9 else datetime.now().weekday())).lower()
-    out = f"Scheduled animes for {day.capitalize()}\n\n"
-    data = requests.get(base_url+day).json()
-    sched_ls =  data["data"]
-    for i in sched_ls:
-        try:
-            title = i['titles'][0]['title']
-        except IndexError:
-            title = i['title']
-        out += f"• `{title}`\n"
-    return out, x if x!=9 else datetime.now().weekday()
+@pgram.on_message(filters.command('schedule'))
+def schedule(_, message):
+    results = requests.get('https://subsplease.org/api/?f=schedule&h=true&tz=Japan').json()
+    text = None
+    for result in results['schedule']:
+        title = result['title']
+        time = result['time']
+        aired = bool(result['aired'])
+        title = f"**[{title}](https://subsplease.org/shows/{result['page']})**" if not aired else f"**~~[{title}](https://subsplease.org/shows/{result['page']})~~**"
+        data = f"{title} - **{time}**"
+        
+        if text:
+            text = f"{text}\n{data}"
+        else:
+            text = data
 
-@pgram.on_message(filters.command("schedule"))
-async def _schedule(app : Client, message : Message,data : dict):
-    gid = data['chat']['id']     
-    x = await client.send_message(
-        gid, "<code>Fetching Scheduled Animes</code>"
-    )
-    try:
-        user = data['from_user']['id']
-    except KeyError:
-        user = data['sender_chat']['id']
-    msg = await get_scheduled()
-    buttons = get_btns("SCHEDULED", result=[msg[1]], user=user)
-    await x.edit_text(msg[0], reply_markup=buttons)
-
-
-@pgram.on_callback_query(filters.regex(pattern=r"sched_(.*)"))
-async def ns_(client: Client, cq: CallbackQuery, cdata: dict):
-    kek, day, user = cdata['data'].split("_")
-    msg = await get_scheduled(int(day))
-    buttons = get_btns("SCHEDULED", result=[int(day)], user=user)
-    await cq.edit_message_text(msg[0], reply_markup=buttons)
-
-
+    await message.reply_text(f"**Today's Schedule:**\nTime-Zone: Tokyo (GMT +9)\n\n{text}")
