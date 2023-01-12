6eb21a4e6aff7ab99488bfe9a4835578253af1e1
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-12 18:09:39 +0530

    Update extra.py

--- TeleBot/modules/extra.py
+++ TeleBot/modules/extra.py
@@ -4,17 +4,19 @@ from pyrogram import filters
 
 @pgram.on_message(filters.command("spoiler"))
 async def _spoil(_, message):
+    text = await message.reply("💘 ᴛʀʏɪɴɢ ᴛᴏ ᴍᴀᴋᴇ ɪᴛ sᴘᴏɪʟᴇʀ......")
     replied = message.reply_to_message
     if not replied:
-        return await message.reply_text("`ɪ ᴛʜɪɴᴋ ʏᴏᴜ sʜᴏᴜʟᴅ ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ.`")
+        return await text.edit("`ɪ ᴛʜɪɴᴋ ʏᴏᴜ sʜᴏᴜʟᴅ ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ.`")
     if not replied.photo:
-        return await message.reply_text("`ɪ sᴀɪᴅ ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ ᴍғ.`")
+        return await text.edit("`ʀᴇᴘʟʏ ᴛᴏ ᴀ ɪᴍᴀɢᴇ ᴍғ.`")
     try: 
         file = await replied.download()
         await pgram.send_photo(message.chat.id,photo=file,has_spoiler=True)
         try:
             os.remove(file)
         except Exception as er:
-            return await message.reply_text(er)
+            return await text.edit(er)
+        await text.delete()
     except Exception as er:
-        await message.reply_text(er)
+        await text.edit(er)
