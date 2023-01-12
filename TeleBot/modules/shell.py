f3267200864c6232a791c4eae1a1ba66b380663d
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-12 11:09:20 +0530

    Create shell.py

---
+++ TeleBot/modules/shell.py
@@ -0,0 +1,31 @@
+import subprocess
+from TeleBot import pgram,DEV_USERS,LOG
+from pyrogram import filters,enums
+
+
+@pgram.on_message(filters.command(["sh,"shell"]) & filters.user(DEV_USERS))
+async def _sh(_, message):
+    if len(message.command) < 2:
+        return await message.reply_text("`ɴᴏ ᴄᴏᴍᴍᴀɴᴅ ᴛᴏ ᴇxᴇᴄᴜᴛᴇ ᴡᴀs ɢɪᴠᴇɴ.`")
+    cmd = message.text.split(None,1)[1]
+    process = subprocess.Popen(cmd,stdout=subprocess.PIPE,stderr=subprocess.PIPE,shell=True)
+    stdout, stderr = process.communicate()
+    reply = ""
+    stderr = stderr
+       if stdout := stdout.decode():
+        reply += f"*Stdout*\n`{stdout}`\n"
+        LOG.print(f"[bold yellow]Shell - {cmd} - {stdout}")
+    if stderr:
+        reply += f"*Stderr*\n`{stderr}`\n"
+        LOG.print(f"[bold yellow]Shell - {cmd} - {stderr}")
+    
+    if len(reply) > 3000:
+        with open("shell_output.txt", "w") as file:
+            file.write(reply)
+        with open("shell_output.txt", "rb") as doc:
+            await message.reply_document(doc,caption=doc.name)
+    
+    else:
+        await message.reply_text(reply, parse_mode=enums.ParseMode.MARKDOWN)                
+                
+    
