9c341d0afd027c11c6168645e7bc2288f667a923
sᴛᴇᴠᴇ <120701871+NotStark@users.noreply.github.com>
2023-01-12 09:35:44 +0530

    Update purge.py

--- TeleBot/modules/purge.py
+++ TeleBot/modules/purge.py
@@ -24,3 +24,30 @@ async def _del(_, message):
         await message.delete()
     except:
         pass        
+
+@pgram.on_message(filters.command("purge"))
+@bot_admin
+@user_admin
+@bot_can_del
+@user_can_del
+async def _del(_, message):
+    if message.sender_chat:
+        return
+    replied = message.reply_to_message
+    if not replied:
+        return await message.reply_text("**💌 ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴍᴇssᴀɢᴇ ᴛᴏ sᴇʟᴇᴄᴛ ᴡʜᴇʀᴇ ᴛᴏ sᴛᴀʀᴛ ᴘᴜʀɢɪɴɢ ғʀᴏᴍ**.")
+    message_id = replied.id + 1
+    del_to = message.id
+    del_list = []
+    for i in range(message_id,del_to):
+        del_list.append(i)
+        if len(del_list) == 100:
+            await pgram.delete_messages(message.chat.id, del_list)
+            del_list = []             
+            print(del_list)
+
+
+
+
+
+
